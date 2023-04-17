c8db7a1ee95cca11b53aee7c573b8c266fd903d0
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2023-04-17 18:32:03 +0000

    chore: adding mean absolute error metric
    following the mse code, writing the mae metric

--- keras_core/metrics/regression_metrics.py
+++ keras_core/metrics/regression_metrics.py
@@ -40,3 +40,16 @@ class MeanSquareError(reduction_metrics.MeanMetricWrapper):
 
     def get_config(self):
         return {"name": self.name, "dtype": self.dtype}
+
+
+def mean_absolute_error(y_true, y_pred):
+    ndim = len(y_pred.shape)
+    return ops.mean(ops.abs(y_true - y_pred), axis=list(range(1, ndim)))
+
+
+class MeanAbsoluteError(reduction_metrics.MeanMetricWrapper):
+    def __init__(self, name="mean_absolute_error", dtype=None):
+        super().__init__(fn=mean_absolute_error, name=name, dtype=dtype)
+
+    def get_config(self):
+        return {"name": self.name, "dtype": self.dtype}
