a677f2f0ff9cdbc93d27fc003bb98ce24aae822a
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2023-04-19 15:00:23 -0400

    Backend import merge conflict

--- keras_core/backend/__init__.py
+++ keras_core/backend/__init__.py
@@ -1,8 +1,9 @@
 import json
 import os
 
-from keras_core.backend.common import standardize_dtype
-from keras_core.backend.common import standardize_shape
+from keras_core.backend.common.variables import standardize_dtype
+from keras_core.backend.common.variables import standardize_shape
+from keras_core.backend.config import backend
 from keras_core.backend.config import epsilon
 from keras_core.backend.config import floatx
 from keras_core.backend.config import image_data_format
@@ -13,6 +14,8 @@ from keras_core.backend.keras_tensor import KerasTensor
 from keras_core.backend.keras_tensor import any_symbolic_tensors
 from keras_core.backend.keras_tensor import is_keras_tensor
 from keras_core.backend.stateless_scope import StatelessScope
+from keras_core.backend.stateless_scope import get_stateless_scope
+from keras_core.backend.stateless_scope import in_stateless_scope
 from keras_core.utils.io_utils import print_msg
 
 # Import backend functions.
