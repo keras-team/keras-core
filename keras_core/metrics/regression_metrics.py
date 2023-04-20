128e048a51f5b55d9ef693b4990537a214487cf6
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2023-04-20 15:48:02 -0400

    Add docstring

--- keras_core/metrics/regression_metrics.py
+++ keras_core/metrics/regression_metrics.py
@@ -5,6 +5,19 @@ from keras_core.metrics.metric import Metric
 
 
 class MeanSquaredError(reduction_metrics.MeanMetricWrapper):
+    """Computes the mean squared error between `y_true` and `y_pred`.
+
+    Args:
+        name: (Optional) string name of the metric instance.
+        dtype: (Optional) data type of the metric result.
+
+    Standalone usage:
+
+    >>> m = keras_core.metrics.MeanSquaredError()
+    >>> m.update_state([[0, 1], [0, 0]], [[1, 1], [0, 0]])
+    >>> m.result()
+    0.25
+    """
     def __init__(self, name="mean_squared_error", dtype=None):
         super().__init__(fn=losses.mean_squared_error, name=name, dtype=dtype)
 
