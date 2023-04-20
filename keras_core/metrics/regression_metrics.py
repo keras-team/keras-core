d93d06fd4881b73732733b36496797227a29f27c
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2023-04-20 15:45:03 -0400

    Add MeanSquaredError and initial testing

--- keras_core/metrics/regression_metrics.py
+++ keras_core/metrics/regression_metrics.py
@@ -1,15 +1,13 @@
 from keras_core import operations as ops
+from keras_core.losses import losses
 from keras_core.metrics import reduction_metrics
-
-
-def mean_squared_error(y_true, y_pred):
-    ndim = len(y_pred.shape)
-    return ops.mean((y_true - y_pred) ** 2, axis=list(range(1, ndim)))
+from keras_core.metrics.metric import Metric
 
 
 class MeanSquaredError(reduction_metrics.MeanMetricWrapper):
     def __init__(self, name="mean_squared_error", dtype=None):
-        super().__init__(fn=mean_squared_error, name=name, dtype=dtype)
+        super().__init__(fn=losses.mean_squared_error, name=name, dtype=dtype)
 
     def get_config(self):
+        # TODO: placeholder, this should be refactored into base class
         return {"name": self.name, "dtype": self.dtype}
