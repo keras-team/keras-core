25dec9e7137ca37d53c0d41bb771e4072aec402b
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-04-21 22:47:14 +0000

    reduce line length limit from 200 to 80

--- keras_core/backend/jax/trainer.py
+++ keras_core/backend/jax/trainer.py
@@ -214,7 +214,8 @@ class JAXTrainer(base_trainer.Trainer):
                     break
 
             # Update variable values
-            # NOTE: doing this after each step would be a big performance bottleneck.
+            # NOTE: doing this after each step would be a big performance
+            # bottleneck.
             for ref_v, v in zip(self.trainable_variables, trainable_variables):
                 ref_v.assign(v)
             for ref_v, v in zip(
@@ -399,7 +400,8 @@ class JAXTrainer(base_trainer.Trainer):
                 metrics_variables,
             )
             logs, state = test_step(state, data)
-            # Note that trainable variables are not returned since they're immutable here.
+            # Note that trainable variables are not returned since they're
+            # immutable here.
             non_trainable_variables, metrics_variables = state
 
             callbacks.on_test_batch_end(step, logs)
