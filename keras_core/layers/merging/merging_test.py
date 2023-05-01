de43524990fda7e1ff498da12bb7b9959487372d
AakashKumarNain <aakashnain@outlook.com>
2023-05-01 12:53:02 +0530

    refactor test class

--- keras_core/layers/merging/merging_test.py
+++ keras_core/layers/merging/merging_test.py
@@ -8,30 +8,29 @@ from keras_core import operations as ops
 from keras_core import testing
 
 
-@pytest.mark.skipif(
-    backend.backend() != "tensorflow",
-    reason="Dynamic shapes are only supported in TensorFlow backend.",
-)
-class MergingLayersDynamicShapeTest(testing.TestCase):
-    def test_add(self):
-        batch_size = 2
-        shape = (4, 5)
-        x1 = np.random.rand(batch_size, *shape)
-        x2 = np.random.rand(batch_size, *shape)
-        x3 = ops.convert_to_tensor(x1 + x2)
-
+class MergingLayersTest(testing.TestCase):
+    def test_add_basic(self):
         self.run_layer_test(
             layers.Add,
             init_kwargs={},
-            input_shape=None,
-            input_data=[x1, x2],
-            expected_output=x3,
-            expected_num_trainable_weights=None,
-            expected_num_non_trainable_weights=None,
-            expected_num_seed_generators=None,
-            expected_num_losses=None,
+            input_shape=[(2, 3), (2, 3)],
+            expected_output_shape=(2, 3),
+            expected_num_trainable_weights=0,
+            expected_num_non_trainable_weights=0,
+            expected_num_seed_generators=0,
+            expected_num_losses=0,
             supports_masking=True,
         )
+
+    @pytest.mark.skipif(
+        not backend.DYNAMIC_SHAPES_OK,
+        reason="Dynamic shapes are only supported in TensorFlow backend.",
+    )
+    def test_add_correctness_dynamic(self):
+        shape = (2, 4, 5)
+        x1 = np.random.rand(*shape)
+        x2 = np.random.rand(*shape)
+        x3 = ops.convert_to_tensor(x1 + x2)
 
         input_1 = layers.Input(shape=shape)
         input_2 = layers.Input(shape=shape)
@@ -40,7 +39,7 @@ class MergingLayersDynamicShapeTest(testing.TestCase):
         model = models.Model([input_1, input_2], out)
         res = model([x1, x2])
 
-        self.assertEqual(res.shape, (batch_size, *shape))
+        self.assertEqual(res.shape, shape)
         self.assertAllClose(res, x3, atol=1e-4)
         self.assertIsNone(
             add_layer.compute_mask([input_1, input_2], [None, None])
@@ -54,38 +53,12 @@ class MergingLayersDynamicShapeTest(testing.TestCase):
             )
         )
 
-        with self.assertRaisesRegex(ValueError, "`mask` should be a list."):
-            add_layer.compute_mask([input_1, input_2], x1)
-
-        with self.assertRaisesRegex(ValueError, "`inputs` should be a list."):
-            add_layer.compute_mask(input_1, [None, None])
-
-        with self.assertRaisesRegex(
-            ValueError, " should have the same length."
-        ):
-            add_layer.compute_mask([input_1, input_2], [None])
-
-
-class MergingLayersStaticShapeTest(testing.TestCase):
-    def test_add(self):
+    def test_add_correctness_static(self):
         batch_size = 2
         shape = (4, 5)
         x1 = np.random.rand(batch_size, *shape)
         x2 = np.random.rand(batch_size, *shape)
         x3 = ops.convert_to_tensor(x1 + x2)
-
-        self.run_layer_test(
-            layers.Add,
-            init_kwargs={},
-            input_shape=None,
-            input_data=[x1, x2],
-            expected_output=x3,
-            expected_num_trainable_weights=None,
-            expected_num_non_trainable_weights=None,
-            expected_num_seed_generators=None,
-            expected_num_losses=None,
-            supports_masking=True,
-        )
 
         input_1 = layers.Input(shape=shape, batch_size=batch_size)
         input_2 = layers.Input(shape=shape, batch_size=batch_size)
@@ -107,6 +80,16 @@ class MergingLayersStaticShapeTest(testing.TestCase):
                 )
             )
         )
+
+    def test_add_errors(self):
+        batch_size = 2
+        shape = (4, 5)
+        x1 = np.random.rand(batch_size, *shape)
+        x2 = np.random.rand(batch_size, *shape)
+
+        input_1 = layers.Input(shape=shape, batch_size=batch_size)
+        input_2 = layers.Input(shape=shape, batch_size=batch_size)
+        add_layer = layers.Add()
 
         with self.assertRaisesRegex(ValueError, "`mask` should be a list."):
             add_layer.compute_mask([input_1, input_2], x1)
