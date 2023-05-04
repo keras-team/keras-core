8d1678606b8cbf4cd5dc869cd5b1f60f050abb21
AakashKumarNain <aakashnain@outlook.com>
2023-05-04 12:26:45 +0530

    fix tests

--- keras_core/layers/merging/merging_test.py
+++ keras_core/layers/merging/merging_test.py
@@ -669,7 +669,7 @@ class MergingLayersTest(testing.TestCase):
             expected_num_non_trainable_weights=0,
             expected_num_seed_generators=0,
             expected_num_losses=0,
-            supports_masking=False,
+            supports_masking=None,
         )
 
     @pytest.mark.skipif(
