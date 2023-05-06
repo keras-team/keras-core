553112b03789dbe3938ae1eea4c396a690df34eb
sampathweb <1437573+sampathweb@users.noreply.github.com>
2023-05-05 22:47:08 -0500

    Update CSV Logger docstring

--- keras_core/callbacks/csv_logger.py
+++ keras_core/callbacks/csv_logger.py
@@ -16,18 +16,18 @@ class CSVLogger(Callback):
     Supports all values that can be represented as a string,
     including 1D iterables such as `np.ndarray`.
 
+    Args:
+        filename: Filename of the CSV file, e.g. `'run/log.csv'`.
+        separator: String used to separate elements in the CSV file.
+        append: Boolean. True: append if file exists (useful for continuing
+            training). False: overwrite existing file.
+
     Example:
 
     ```python
     csv_logger = CSVLogger('training.log')
     model.fit(X_train, Y_train, callbacks=[csv_logger])
     ```
-
-    Args:
-        filename: Filename of the CSV file, e.g. `'run/log.csv'`.
-        separator: String used to separate elements in the CSV file.
-        append: Boolean. True: append if file exists (useful for continuing
-            training). False: overwrite existing file.
     """
 
     def __init__(self, filename, separator=",", append=False):
