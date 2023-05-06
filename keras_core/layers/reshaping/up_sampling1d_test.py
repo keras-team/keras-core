175de8eb6fe7578f26454ff3a9a26b1a1d8de2d5
Rick Chao <rchao@google.com>
2023-05-05 17:53:18 -0700

    Fix testing test.

--- keras_core/layers/reshaping/up_sampling1d_test.py
+++ keras_core/layers/reshaping/up_sampling1d_test.py
@@ -9,11 +9,3 @@ class UpSamplingTest(testing.TestCase):
             init_kwargs={"size": 2},
             input_shape=(3, 5, 4),
         )
-
-    def test_try(self):
-    	input_shape = (2, 2, 3)
-    	import numpy as np
-    	x = np.arange(np.prod(input_shape)).reshape(input_shape)
-    	y = layers.UpSampling1D(size=2)(x)
-    	import pdb; pdb.set_trace()
-
