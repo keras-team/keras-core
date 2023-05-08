92c38c901cff10160629ac6f87a4ae513f435b75
Qianli Scott Zhu <scottzhu@google.com>
2023-05-08 18:58:33 +0000

    Skip the test case for non-tf backend

--- keras_core/backend/tensorflow/distribute_test.py
+++ keras_core/backend/tensorflow/distribute_test.py
@@ -1,5 +1,6 @@
 """Tests for tf.distribute related functionality under tf implementation."""
 
+import pytest
 import tensorflow as tf
 
 from keras_core import backend
@@ -8,14 +9,14 @@ from keras_core import models
 from keras_core import testing
 
 
+@pytest.mark.skipif(
+    backend.backend() != 'tensorflow',
+    reason="'The distribute test can only run with TF backend.",
+)
 class DistributeTest(testing.TestCase):
 
     def setUp(self):
         super().setUp()
-        if backend.backend() != 'tensorflow':
-            raise ValueError(
-                'The distribute test can only run with TF backend, '
-                f'current backend is {backend.backend()}')
         # Need at least 2 devices for distribution related tests.
         cpus = tf.config.list_physical_devices("CPU")
         tf.config.set_logical_device_configuration(
