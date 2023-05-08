48786229fc437350edf81e3d7d6be353ffed2b2d
divyashreepathihalli <divyasreepat@google.com>
2023-05-08 20:16:20 +0000

    update docstring

--- keras_core/layers/activations/leaky_relu.py
+++ keras_core/layers/activations/leaky_relu.py
@@ -7,7 +7,7 @@ from keras_core.layers.layer import Layer
 class LeakyReLU(Layer):
     """Leaky version of a Rectified Linear Unit activation layer.
 
-    It allows a small gradient when the unit is not active:
+    The layer allows a small gradient when the unit is not active.
 
     Formula:
     ``` python
@@ -17,11 +17,17 @@ class LeakyReLU(Layer):
 
     Example:
     ``` python
+    leaky_relu_layer = LeakyReLU(negative_slope=0.5)
+    input = np.array([-10, -5, 0.0, 5, 10])
+    result = leaky_relu_layer(input)
+    # result = [-5. , -2.5,  0. ,  5. , 10.]
     ```
 
     Args:
         negative_slope: Float >= `0.`. Negative slope coefficient.
           Defaults to `0.3`.
+        **kwargs: Base layer keyword arguments, such as
+            `name` and `dtype`.
 
     """
 
