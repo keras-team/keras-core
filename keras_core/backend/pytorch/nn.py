5fa57c5d05abfc7b1f922675d2282639470dea11
sampathweb <1437573+sampathweb@users.noreply.github.com>
2023-05-08 15:51:40 -0500

    Add PyTorch nn functions

--- keras_core/backend/pytorch/nn.py
+++ keras_core/backend/pytorch/nn.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 import torch.nn.functional as F
 
@@ -69,61 +68,6 @@ def log_softmax(x, axis=-1):
     return F.log_softmax(x, dim=axis)
 
 
-def _convert_to_spatial_operand(
-    x,
-    num_spatial_dims,
-    data_format="channels_last",
-    include_batch_and_channels=True,
-):
-    # Helper function that converts an operand to a spatial operand.
-    x = (x,) * num_spatial_dims if isinstance(x, int) else x
-    if not include_batch_and_channels:
-        return x
-    if data_format == "channels_last":
-        x = (1,) + x + (1,)
-    else:
-        x = (1,) + (1,) + x
-    return x
-
-
-def _pool(
-    inputs,
-    initial_value,
-    reduce_fn,
-    pool_size,
-    strides=None,
-    padding="valid",
-):
-    """Helper function to define pooling functions.
-
-    Args:
-        inputs: input data of shape `N+2`.
-        initial_value: the initial value for the reduction.
-        reduce_fn: a reduce function of the form `(T, T) -> T`.
-        pool_size: a sequence of `N` integers, representing the window size to
-            reduce over.
-        strides: a sequence of `N` integers, representing the inter-window
-            strides (default: `(1, ..., 1)`).
-        padding: either the string `same` or `valid`.
-
-    Returns:
-        The output of the reduction for each window slice.
-    """
-    if padding not in ("same", "valid"):
-        raise ValueError(
-            f"Invalid padding '{padding}', must be 'same' or 'valid'."
-        )
-    padding = padding.upper()
-    return lax.reduce_window(
-        inputs,
-        initial_value,
-        reduce_fn,
-        pool_size,
-        strides,
-        padding,
-    )
-
-
 def max_pool(
     inputs,
     pool_size,
@@ -131,41 +75,7 @@ def max_pool(
     padding="valid",
     data_format="channels_last",
 ):
-    strides = pool_size if strides is None else strides
-    padding = padding.upper()
-    tf_data_format = _convert_data_format("channels_last", len(inputs.shape))
-    if data_format == "channels_first":
-        # Tensorflow pooling does not support `channels_first` format, so
-        # we need to transpose to `channels_last` format.
-        inputs = _transpose_spatial_inputs(inputs)
-
-    outputs = tf.nn.max_pool(
-        inputs,
-        pool_size,
-        strides,
-        padding,
-        tf_data_format,
-    )
-    if data_format == "channels_first":
-        outputs = _transpose_spatial_outputs(outputs)
-    return outputs
-
-def max_pool(
-    inputs,
-    pool_size,
-    strides=None,
-    padding="valid",
-    data_format="channels_last",
-):
-    num_spatial_dims = inputs.ndim - 2
-    pool_size = _convert_to_spatial_operand(
-        pool_size, num_spatial_dims, data_format
-    )
-    strides = pool_size if strides is None else strides
-    strides = _convert_to_spatial_operand(
-        strides, num_spatial_dims, data_format
-    )
-    return _pool(inputs, -jnp.inf, lax.max, pool_size, strides, padding)
+    raise NotImplementedError("`max_pool` not yet implemeneted for PyTorch Backend")
 
 
 def average_pool(
@@ -175,61 +85,7 @@ def average_pool(
     padding,
     data_format="channels_last",
 ):
-    num_spatial_dims = inputs.ndim - 2
-    pool_size = _convert_to_spatial_operand(
-        pool_size, num_spatial_dims, data_format
-    )
-    strides = pool_size if strides is None else strides
-    strides = _convert_to_spatial_operand(
-        strides, num_spatial_dims, data_format
-    )
-
-    pooled = _pool(inputs, 0.0, lax.add, pool_size, strides, padding)
-    if padding == "valid":
-        # Avoid the extra reduce_window.
-        return pooled / np.prod(pool_size)
-    else:
-        # Count the number of valid entries at each input point, then use that
-        # for computing average. Assumes that any two arrays of same shape will
-        # be padded the same. Avoid broadcasting on axis where pooling is
-        # skipped.
-        shape = [
-            (a if b != 1 else 1) for (a, b) in zip(inputs.shape, pool_size)
-        ]
-        window_counts = _pool(
-            jnp.ones(shape, inputs.dtype),
-            0.0,
-            lax.add,
-            pool_size,
-            strides,
-            padding,
-        )
-        return pooled / window_counts
-
-
-def _convert_to_lax_conv_dimension_numbers(
-    num_spatial_dims,
-    data_format="channels_last",
-    transpose=False,
-):
-    """Create a `lax.ConvDimensionNumbers` for the given inputs."""
-    num_dims = num_spatial_dims + 2
-
-    if data_format == "channels_last":
-        spatial_dims = tuple(range(1, num_dims - 1))
-        inputs_dn = (0, num_dims - 1) + spatial_dims
-    else:
-        spatial_dims = tuple(range(2, num_dims))
-        inputs_dn = (0, 1) + spatial_dims
-
-    if transpose:
-        kernel_dn = (num_dims - 2, num_dims - 1) + tuple(range(num_dims - 2))
-    else:
-        kernel_dn = (num_dims - 1, num_dims - 2) + tuple(range(num_dims - 2))
-
-    return lax.ConvDimensionNumbers(
-        lhs_spec=inputs_dn, rhs_spec=kernel_dn, out_spec=inputs_dn
-    )
+    raise NotImplementedError("`average_pool` not yet implemeneted for PyTorch Backend")
 
 
 def conv(
@@ -240,45 +96,7 @@ def conv(
     data_format="channels_last",
     dilation_rate=1,
 ):
-    num_spatial_dims = inputs.ndim - 2
-    dimension_numbers = _convert_to_lax_conv_dimension_numbers(
-        num_spatial_dims,
-        data_format,
-        transpose=False,
-    )
-    strides = _convert_to_spatial_operand(
-        strides,
-        num_spatial_dims,
-        data_format,
-        include_batch_and_channels=False,
-    )
-    dilation_rate = _convert_to_spatial_operand(
-        dilation_rate,
-        num_spatial_dims,
-        data_format,
-        include_batch_and_channels=False,
-    )
-    if data_format == "channels_last":
-        channels = inputs.shape[-1]
-    else:
-        channels = inputs.shape[1]
-    kernel_in_channels = kernel.shape[-2]
-    if channels % kernel_in_channels > 0:
-        raise ValueError(
-            "The number of input channels must be evenly divisible by "
-            f"kernel's in_channels. Received input channels {channels} and "
-            f"kernel in_channels {kernel_in_channels}. "
-        )
-    feature_group_count = channels // kernel_in_channels
-    return jax.lax.conv_general_dilated(
-        inputs,
-        kernel,
-        strides,
-        padding,
-        rhs_dilation=dilation_rate,
-        dimension_numbers=dimension_numbers,
-        feature_group_count=feature_group_count,
-    )
+    raise NotImplementedError("`conv` not yet implemeneted for PyTorch Backend")
 
 
 def depthwise_conv(
@@ -289,40 +107,7 @@ def depthwise_conv(
     data_format="channels_last",
     dilation_rate=1,
 ):
-    num_spatial_dims = inputs.ndim - 2
-    dimension_numbers = _convert_to_lax_conv_dimension_numbers(
-        num_spatial_dims,
-        data_format,
-        transpose=False,
-    )
-    strides = _convert_to_spatial_operand(
-        strides,
-        num_spatial_dims,
-        data_format,
-        include_batch_and_channels=False,
-    )
-    dilation_rate = _convert_to_spatial_operand(
-        dilation_rate,
-        num_spatial_dims,
-        data_format,
-        include_batch_and_channels=False,
-    )
-    feature_group_count = (
-        inputs.shape[-1] if data_format == "channels_last" else inputs.shape[1]
-    )
-    kernel = jnp.reshape(
-        kernel,
-        kernel.shape[:-2] + (1, feature_group_count * kernel.shape[-1]),
-    )
-    return jax.lax.conv_general_dilated(
-        inputs,
-        kernel,
-        strides,
-        padding,
-        rhs_dilation=dilation_rate,
-        dimension_numbers=dimension_numbers,
-        feature_group_count=feature_group_count,
-    )
+    raise NotImplementedError("`depthwise_conv` not yet implemeneted for PyTorch Backend")
 
 
 def separable_conv(
@@ -361,49 +146,18 @@ def conv_transpose(
     data_format="channels_last",
     dilation_rate=1,
 ):
-    num_spatial_dims = inputs.ndim - 2
-    dimension_numbers = _convert_to_lax_conv_dimension_numbers(
-        num_spatial_dims,
-        data_format,
-        transpose=False,
-    )
-    strides = _convert_to_spatial_operand(
-        strides,
-        num_spatial_dims,
-        data_format,
-        include_batch_and_channels=False,
-    )
-    dilation_rate = _convert_to_spatial_operand(
-        dilation_rate,
-        num_spatial_dims,
-        data_format,
-        include_batch_and_channels=False,
-    )
-
-    if output_padding is not None:
-        raise ValueError(
-            "Custom `output_padding` is not supported yet, please set "
-            "`output_padding=None`."
-        )
-    padding = padding.upper()
-    return jax.lax.conv_transpose(
-        inputs,
-        kernel,
-        strides,
-        padding,
-        rhs_dilation=dilation_rate,
-        dimension_numbers=dimension_numbers,
-        transpose_kernel=True,
-    )
+    raise NotImplementedError("`conv_transpose` not yet implemeneted for PyTorch backend")
 
 
 def one_hot(x, num_classes, axis=-1):
-    return jnn.one_hot(x, num_classes, axis=axis)
+    if axis != -1 or axis != x.shape[-1]:
+        raise ValueError("`one_hot` is only implemented for last axis for PyTorch backend")
+    return F.one_hot(x, num_classes)
 
 
 def categorical_crossentropy(target, output, from_logits=False, axis=-1):
-    target = jnp.array(target)
-    output = jnp.array(output)
+    target = torch.as_tensor(target)
+    output = torch.as_tensor(output)
 
     if target.shape != output.shape:
         raise ValueError(
@@ -419,19 +173,20 @@ def categorical_crossentropy(target, output, from_logits=False, axis=-1):
         )
 
     if from_logits:
-        log_prob = jax.nn.log_softmax(output, axis=axis)
+        log_prob = F.log_softmax(output, dim=axis)
     else:
-        output = output / jnp.sum(output, axis, keepdims=True)
-        output = jnp.clip(output, epsilon(), 1.0 - epsilon())
-        log_prob = jnp.log(output)
-    return -jnp.sum(target * log_prob, axis=axis)
+        output = output / torch.sum(output, dim=axis, keepdim=True)
+        output = torch.clip(output, epsilon(), 1.0 - epsilon())
+        log_prob = torch.log(output)
+    return -torch.sum(target * log_prob, dim=axis)
 
 
 def sparse_categorical_crossentropy(target, output, from_logits=False, axis=-1):
-    target = jnp.array(target, dtype="int64")
-    output = jnp.array(output)
+    target = torch.as_tensor(target, dtype=torch.long)
+    output = torch.as_tensor(output)
+
     if len(target.shape) == len(output.shape) and target.shape[-1] == 1:
-        target = jnp.squeeze(target, axis=-1)
+        target = torch.squeeze(target, dim=-1)
 
     if len(output.shape) < 1:
         raise ValueError(
@@ -446,19 +201,19 @@ def sparse_categorical_crossentropy(target, output, from_logits=False, axis=-1):
             f"target.shape={target.shape}, output.shape={output.shape}"
         )
     if from_logits:
-        log_prob = jax.nn.log_softmax(output, axis=axis)
+        log_prob = F.log_softmax(output, dim=axis)
     else:
-        output = output / jnp.sum(output, axis, keepdims=True)
-        output = jnp.clip(output, epsilon(), 1.0 - epsilon())
-        log_prob = jnp.log(output)
-    target = jnn.one_hot(target, output.shape[axis], axis=axis)
-    return -jnp.sum(target * log_prob, axis=axis)
+        output = output / torch.sum(output, dim=axis, keepdim=True)
+        output = torch.clip(output, epsilon(), 1.0 - epsilon())
+        log_prob = torch.log(output)
+    target = one_hot(target, output.shape[axis], axis=axis)
+    return -torch.sum(target * log_prob, dim=axis)
 
 
 def binary_crossentropy(target, output, from_logits=False):
-    # TODO: `torch.Tensor` has device arg. Need to think how to pass it.
-    target = torch.Tensor(target)
-    output = torch.Tensor(output)
+    # TODO: `torch.as_tensor` has device arg. Need to think how to pass it.
+    target = torch.as_tensor(target)
+    output = torch.as_tensor(output)
 
     if target.shape != output.shape:
         raise ValueError(
