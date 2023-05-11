1f590f2a67d7862f2f55ee59cb9a33dca06662b1
divyashreepathihalli <divyasreepat@google.com>
2023-05-11 21:33:34 +0000

    code reformat

--- keras_core/layers/preprocessing/random_flip.py
+++ keras_core/layers/preprocessing/random_flip.py
@@ -2,7 +2,6 @@ import numpy as np
 import tensorflow as tf
 
 from keras_core import backend
-from keras_core import utils
 from keras_core.api_export import keras_core_export
 from keras_core.layers.layer import Layer
 
