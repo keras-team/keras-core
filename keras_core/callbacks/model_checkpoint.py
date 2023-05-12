b38de35c128f80a3987f31a62486786e155d38ee
sampathweb <1437573+sampathweb@users.noreply.github.com>
2023-05-12 15:38:57 -0500

    Adds Model Checkpoint

--- keras_core/callbacks/model_checkpoint.py
+++ keras_core/callbacks/model_checkpoint.py
@@ -56,56 +56,57 @@ class ModelCheckpoint(Callback):
 
     Args:
         filepath: string or `PathLike`, path to save the model file. e.g.
-          filepath = os.path.join(working_dir, 'ckpt', file_name). `filepath`
-          can contain named formatting options, which will be filled the value
-          of `epoch` and keys in `logs` (passed in `on_epoch_end`). For example:
-          if `filepath` is `{epoch:02d}-{val_loss:.2f}.weights.h5`, then the
-          model checkpoints will be saved with the epoch number and the
-          validation loss in the filename. The directory of the filepath should
-          not be reused by any other callbacks to avoid conflicts.
+            filepath = os.path.join(working_dir, 'ckpt', file_name). `filepath`
+            can contain named formatting options, which will be filled the
+            value of `epoch` and keys in `logs` (passed in `on_epoch_end`).
+            For example:
+            if `filepath` is `{epoch:02d}-{val_loss:.2f}.weights.h5`, then the
+            model checkpoints will be saved with the epoch number and the
+            validation loss in the filename. The directory of the filepath
+            should not be reused by any other callbacks to avoid conflicts.
         monitor: The metric name to monitor. Typically the metrics are set by
-          the `Model.compile` method. Note:
+            the `Model.compile` method. Note:
 
-          * Prefix the name with `"val_`" to monitor validation metrics.
-          * Use `"loss"` or "`val_loss`" to monitor the model's total loss.
-          * If you specify metrics as strings, like `"accuracy"`, pass the same
-            string (with or without the `"val_"` prefix).
-          * If you pass `metrics.Metric` objects, `monitor` should be set to
-            `metric.name`
-          * If you're not sure about the metric names you can check the contents
-            of the `history.history` dictionary returned by
-            `history = model.fit()`
-          * Multi-output models set additional prefixes on the metric names.
+            * Prefix the name with `"val_`" to monitor validation metrics.
+            * Use `"loss"` or "`val_loss`" to monitor the model's total loss.
+            * If you specify metrics as strings, like `"accuracy"`, pass the
+                same string (with or without the `"val_"` prefix).
+            * If you pass `metrics.Metric` objects, `monitor` should be set to
+                `metric.name`
+            * If you're not sure about the metric names you can check the
+                contents of the `history.history` dictionary returned by
+                `history = model.fit()`
+            * Multi-output models set additional prefixes on the metric names.
 
         verbose: Verbosity mode, 0 or 1. Mode 0 is silent, and mode 1
-          displays messages when the callback takes an action.
+            displays messages when the callback takes an action.
         save_best_only: if `save_best_only=True`, it only saves when the model
-          is considered the "best" and the latest best model according to the
-          quantity monitored will not be overwritten. If `filepath` doesn't
-          contain formatting options like `{epoch}` then `filepath` will be
-          overwritten by each new better model.
+            is considered the "best" and the latest best model according to the
+            quantity monitored will not be overwritten. If `filepath` doesn't
+            contain formatting options like `{epoch}` then `filepath` will be
+            overwritten by each new better model.
         mode: one of {'auto', 'min', 'max'}. If `save_best_only=True`, the
-          decision to overwrite the current save file is made based on either
-          the maximization or the minimization of the monitored quantity.
-          For `val_acc`, this should be `max`, for `val_loss` this should be
-          `min`, etc. In `auto` mode, the mode is set to `max` if the quantities
-          monitored are 'acc' or start with 'fmeasure' and are set to `min` for
-          the rest of the quantities.
+            decision to overwrite the current save file is made based on either
+            the maximization or the minimization of the monitored quantity.
+            For `val_acc`, this should be `max`, for `val_loss` this should be
+            `min`, etc. In `auto` mode, the mode is set to `max` if the
+            quantities monitored are 'acc' or start with 'fmeasure' and are set
+            to `min` for the rest of the quantities.
         save_weights_only: if True, then only the model's weights will be saved
-          (`model.save_weights(filepath)`), else the full model is saved
-          (`model.save(filepath)`).
+            (`model.save_weights(filepath)`), else the full model is saved
+            (`model.save(filepath)`).
         save_freq: `'epoch'` or integer. When using `'epoch'`, the callback
-          saves the model after each epoch. When using integer, the callback
-          saves the model at end of this many batches. If the `Model` is
-          compiled with `steps_per_execution=N`, then the saving criteria will
-          be checked every Nth batch. Note that if the saving isn't aligned to
-          epochs, the monitored metric may potentially be less reliable (it
-          could reflect as little as 1 batch, since the metrics get reset every
-          epoch). Defaults to `'epoch'`.
+            saves the model after each epoch. When using integer, the callback
+            saves the model at end of this many batches. If the `Model` is
+            compiled with `steps_per_execution=N`, then the saving criteria will
+            be checked every Nth batch. Note that if the saving isn't aligned to
+            epochs, the monitored metric may potentially be less reliable (it
+            could reflect as little as 1 batch, since the metrics get reset
+            every epoch). Defaults to `'epoch'`.
         initial_value_threshold: Floating point initial "best" value of the
-          metric to be monitored. Only applies if `save_best_value=True`. Only
-          overwrites the model weights already saved if the performance of
-          current model is better than this value.
+            metric to be monitored. Only applies if `save_best_value=True`. Only
+            overwrites the model weights already saved if the performance of
+            current model is better than this value.
     """
 
     def __init__(
