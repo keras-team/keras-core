55f7586c52158f021a20c3524ab5d9edc1a06f2f
Jonathan Bischof <jbischof1@gmail.com>
2023-05-12 23:46:14 +0000

    Import `torch` early to avoid segfault

--- keras_core/backend/__init__.py
+++ keras_core/backend/__init__.py
@@ -1,6 +1,11 @@
 import json
 import os
 
+# TODO(jbischof): Figure out why this import order hack is needed
+from keras_core.backend.config import backend
+if backend() == "pytorch":
+    import torch
+
 from keras_core.backend.common.keras_tensor import KerasTensor
 from keras_core.backend.common.keras_tensor import any_symbolic_tensors
 from keras_core.backend.common.keras_tensor import is_keras_tensor
@@ -12,7 +17,6 @@ from keras_core.backend.common.variables import get_autocast_scope
 from keras_core.backend.common.variables import is_float_dtype
 from keras_core.backend.common.variables import standardize_dtype
 from keras_core.backend.common.variables import standardize_shape
-from keras_core.backend.config import backend
 from keras_core.backend.config import epsilon
 from keras_core.backend.config import floatx
 from keras_core.backend.config import image_data_format
@@ -21,6 +25,7 @@ from keras_core.backend.config import set_floatx
 from keras_core.backend.config import set_image_data_format
 from keras_core.backend.config import standardize_data_format
 from keras_core.utils.io_utils import print_msg
+
 
 # Import backend functions.
 if backend() == "tensorflow":
