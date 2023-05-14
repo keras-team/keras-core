c82561c7708b07f63d2e551da65140fc9c63c9d7
AakashKumarNain <aakashnain@outlook.com>
2023-05-14 13:15:18 +0530

    1. Rename TorchDatasetAdapter to TorchDataLoaderAdapter 2. Replace dataset with dataloader 3. Add checks for partial batch size for tensordataset 4. Do inline import

--- keras_core/trainers/data_adapters/torch_data_adapter.py
+++ keras_core/trainers/data_adapters/torch_data_adapter.py
@@ -1,30 +1,46 @@
-import torch
 from tensorflow import nest
 
 from keras_core.trainers.data_adapters.data_adapter import DataAdapter
 
 
-class TorchDatasetAdapter(DataAdapter):
+class TorchDataLoaderAdapter(DataAdapter):
     """Adapter that handles `torch.utils.data.DataLoader`."""
 
-    def __init__(self, dataset, class_weight=None):
-        if not isinstance(dataset, torch.utils.data.DataLoader):
+    def __init__(self, dataloader, class_weight=None):
+        import torch
+
+        if not isinstance(dataloader, torch.utils.data.DataLoader):
             raise ValueError(
-                f"Expected argument `dataset` to be an instance of"
-                f"`torch.utils.data.DataLoader`. Received: {dataset}"
+                f"Expected argument `dataloader` to be an instance of"
+                f"`torch.utils.data.DataLoader`. Received: {dataloader}"
             )
-        self._dataset = dataset
-        self._batch_size = dataset.batch_size
-        self._size = len(dataset)
-        self._partial_batch_size = (
-            self._size * self._batch_size - dataset.sampler.num_samples
-        )
-        ## TODO:
-        ## Map class weights
+        if class_weight:
+            raise ValueError(
+                "`class_weight` is not supported at "
+                "the moment in `TorchDataLoaderAdapter`."
+            )
+
+        self._dataloader = dataloader
+        self._batch_size = dataloader.batch_size
+        self._size = len(dataloader)
+
+        # If DataLoader is created using an instance of `TensorDataset`
+        # then the `num_samples` property for the corresponding sampler
+        # doesn't exist. In that case we will set partail batch size
+        # to `None`.
+        try:
+            self._partial_batch_size = (
+                self._size * self._batch_size - dataloader.sampler.num_samples
+            )
+        except:
+            self._partial_batch_size = None
 
     def get_numpy_iterator(self):
-        for batch in self._dataset:
+        for batch in self._dataloader:
             yield nest.map_structure(lambda x: x.numpy(), batch)
+
+    def get_torch_dataloader(self):
+        return self._dataloader
 
     @property
     def num_batches(self):
@@ -36,7 +52,10 @@ class TorchDatasetAdapter(DataAdapter):
 
     @property
     def has_partial_batch(self):
-        return self._partial_batch_size > 0
+        if self._partial_batch_size:
+            return self._partial_batch_size > 0
+        else:
+            return None
 
     @property
     def partial_batch_size(self):
