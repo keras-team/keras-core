ce517b0891133ed5f78a6b7e0638c1eba2939813
sampathweb <1437573+sampathweb@users.noreply.github.com>
2023-05-15 21:24:13 -0500

    Adds Random Unit Test

--- keras_core/backend/torch/random.py
+++ keras_core/backend/torch/random.py
@@ -1,105 +1,20 @@
-import torch
-import torch.nn.functional as tnn
+from keras_core.backend.config import floatx
+from keras_core.random.seed_generator import SeedGenerator
+from keras_core.random.seed_generator import draw_seed
+from keras_core.random.seed_generator import make_default_seed
 
-from keras_core.backend.common.random import SeedGenerator
-from keras_core.backend.common.random import draw_seed
-from keras_core.backend.common.random import make_default_seed
-
-
-def torch_seed_generator(seed):
-    seed = draw_seed(seed)
-    generator = torch.Generator()
-    generator.manual_seed(seed)
-    return generator
 
 def normal(shape, mean=0.0, stddev=1.0, dtype=None, seed=None):
-    """Produce random number based on the normal distribution.
-
-    Args:
-        shape: The shape of the random values to generate.
-        mean: Floats, defaults to 0. Mean of the random values to generate.
-        stddev: Floats, defaults to 1. Standard deviation of the random values
-            to generate.
-        dtype: Optional dtype of the tensor. Only floating point types are
-            supported. If not specified, `keras.backend.floatx()` is used,
-            which defaults to `float32` unless you configured it otherwise (via
-            `keras.backend.set_floatx(float_dtype)`).
-        seed: A Python integer or instance of
-            `keras_core.backend.SeedGenerator`.
-            Used to make the behavior of the initializer
-            deterministic. Note that an initializer seeded with an integer
-            or None (unseeded) will produce the same random values
-            across multiple calls. To get different random values
-            across multiple calls, use as seed an instance
-            of `keras_core.backend.SeedGenerator`.
-    """
-    dtype = dtype or floatx()
-    generator = torch_seed_generator(seed)
-    return torch.normal(
-        mean, stddev, size=shape, generator=generator, dtype=dtype
-    )
+    raise NotImplementedError
 
 
 def uniform(shape, minval=0.0, maxval=1.0, dtype=None, seed=None):
-    """Produce random number based on the uniform distribution.
-
-    Args:
-        shape: The shape of the random values to generate.
-        minval: Floats, defaults to 0. Lower bound of the range of
-            random values to generate (inclusive).
-        maxval: Floats, defaults to 1. Upper bound of the range of
-            random values to generate (exclusive).
-        dtype: Optional dtype of the tensor. Only floating point types are
-            supported. If not specified, `keras.backend.floatx()` is used,
-            which defaults to `float32` unless you configured it otherwise (via
-            `keras.backend.set_floatx(float_dtype)`)
-        seed: A Python integer or instance of
-            `keras_core.backend.SeedGenerator`.
-            Used to make the behavior of the initializer
-            deterministic. Note that an initializer seeded with an integer
-            or None (unseeded) will produce the same random values
-            across multiple calls. To get different random values
-            across multiple calls, use as seed an instance
-            of `keras_core.backend.SeedGenerator`.
-    """
-    dtype = dtype or floatx()
-    generator = torch_seed_generator(seed)
-    return (maxval - minval) * torch.rand(
-        *shape, generator=generator, dtype=dtype
-    ) + minval
+    raise NotImplementedError
 
 
 def truncated_normal(shape, mean=0.0, stddev=1.0, dtype=None, seed=None):
-    """Produce random number based on the truncated normal distribution.
-
-    Args:
-        shape: The shape of the random values to generate.
-        mean: Floats, defaults to 0. Mean of the random values to generate.
-        stddev: Floats, defaults to 1. Standard deviation of the random values
-            to generate.
-        dtype: Optional dtype of the tensor. Only floating point types are
-            supported. If not specified, `keras.backend.floatx()` is used,
-            which defaults to `float32` unless you configured it otherwise (via
-            `keras.backend.set_floatx(float_dtype)`)
-        seed: A Python integer or instance of
-            `keras_core.backend.SeedGenerator`.
-            Used to make the behavior of the initializer
-            deterministic. Note that an initializer seeded with an integer
-            or None (unseeded) will produce the same random values
-            across multiple calls. To get different random values
-            across multiple calls, use as seed an instance
-            of `keras_core.backend.SeedGenerator`.
-    """
-    dtype = dtype or floatx()
-    _ = torch_seed_generator(seed)
-    # TODO
-    raise NotImplementedError(
-        "`truncated_normal` not implemented in PyTorch backend."
-    )
+    raise NotImplementedError
 
 
 def dropout(inputs, rate, noise_shape=None, seed=None):
-    # TODO: setting seed globally might create side effects.
-    # Need to see how to use `torch_seed_generator`
-    torch.manual_seed(seed)
-    return tnn.dropout(inputs, p=rate)
+    raise NotImplementedError
--- keras_core/random/random_test.py
+++ keras_core/random/random_test.py
@@ -1,21 +1,20 @@
+import numpy as np
 from absl.testing import parameterized
 
-import numpy as np
-
-from keras_core.random import random
+import keras_core.operations as ops
 from keras_core import testing
 from keras_core.backend.common.keras_tensor import KerasTensor
-import keras_core.operations as ops
+from keras_core.operations import numpy as knp
+from keras_core.random import random
 
 
 class RandomTest(testing.TestCase, parameterized.TestCase):
-
     @parameterized.parameters(
-            {"seed": 10, "shape": (5,), "mean": 0, "stddev": 1},
-            {"seed": 10, "shape": (2, 3), "mean": 0, "stddev": 1},
-            {"seed": 10, "shape": (2, 3, 4), "mean": 0, "stddev": 1},
-            {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 1},
-            {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 3},
+        {"seed": 10, "shape": (5,), "mean": 0, "stddev": 1},
+        {"seed": 10, "shape": (2, 3), "mean": 0, "stddev": 1},
+        {"seed": 10, "shape": (2, 3, 4), "mean": 0, "stddev": 1},
+        {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 1},
+        {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 3},
     )
     def test_normal(self, seed, shape, mean, stddev):
         np.random.seed(seed)
@@ -25,11 +24,11 @@ class RandomTest(testing.TestCase, parameterized.TestCase):
         self.assertEqual(res.shape, np_res.shape)
 
     @parameterized.parameters(
-            {"seed": 10, "shape": (5,), "minval": 0, "maxval": 1},
-            {"seed": 10, "shape": (2, 3), "minval": 0, "maxval": 1},
-            {"seed": 10, "shape": (2, 3, 4), "minval": 0, "maxval": 2},
-            {"seed": 10, "shape": (2, 3), "minval": -1, "maxval": 1},
-            {"seed": 10, "shape": (2, 3), "minval": 1, "maxval": 3},
+        {"seed": 10, "shape": (5,), "minval": 0, "maxval": 1},
+        {"seed": 10, "shape": (2, 3), "minval": 0, "maxval": 1},
+        {"seed": 10, "shape": (2, 3, 4), "minval": 0, "maxval": 2},
+        {"seed": 10, "shape": (2, 3), "minval": -1, "maxval": 1},
+        {"seed": 10, "shape": (2, 3), "minval": 1, "maxval": 3},
     )
     def test_uniform(self, seed, shape, minval, maxval):
         np.random.seed(seed)
@@ -41,22 +40,26 @@ class RandomTest(testing.TestCase, parameterized.TestCase):
         self.assertGreaterEqual(ops.max(res), minval)
 
     @parameterized.parameters(
-            {"seed": 10, "shape": (5,), "mean": 0, "stddev": 1},
-            {"seed": 10, "shape": (2, 3), "mean": 0, "stddev": 1},
-            {"seed": 10, "shape": (2, 3, 4), "mean": 0, "stddev": 1},
-            {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 1},
-            {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 3},
+        {"seed": 10, "shape": (5,), "mean": 0, "stddev": 1},
+        {"seed": 10, "shape": (2, 3), "mean": 0, "stddev": 1},
+        {"seed": 10, "shape": (2, 3, 4), "mean": 0, "stddev": 1},
+        {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 1},
+        {"seed": 10, "shape": (2, 3), "mean": 10, "stddev": 3},
     )
     def test_truncated_normal(self, seed, shape, mean, stddev):
         np.random.seed(seed)
         np_res = np.random.normal(loc=mean, scale=stddev, size=shape)
-        res = random.truncated_normal(shape, mean=mean, stddev=stddev, seed=seed)
+        res = random.truncated_normal(
+            shape, mean=mean, stddev=stddev, seed=seed
+        )
         self.assertEqual(res.shape, KerasTensor(shape).shape)
         self.assertEqual(res.shape, np_res.shape)
         self.assertLessEqual(ops.max(res), mean + 2 * stddev)
         self.assertGreaterEqual(ops.max(res), mean - 2 * stddev)
 
-
     def test_dropout(self):
-        # TODO: add dropout test
-        pass
+        x = knp.ones((3, 5))
+        self.assertAllClose(random.dropout(x, rate=0, seed=0), x)
+        x_res = random.dropout(x, rate=0.8, seed=0)
+        self.assertGreater(knp.max(x_res), knp.max(x))
+        self.assertGreater(knp.sum(x_res == 0), 2)
