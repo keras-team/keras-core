1b24426f0b1762b7267c05234c78e7270964fc61
Neel Kovelamudi <nkovela@google.com>
2023-05-17 00:39:16 +0000

    Add tensor conversion to numpy

--- keras_core/backend/torch/numpy.py
+++ keras_core/backend/torch/numpy.py
@@ -1,52 +1,50 @@
 import torch
 from keras_core.backend.torch.core import to_torch_dtype
-
-
-def _make_torch_val(inputs):
-    # This helper converts individual values (int, bool, etc.)
-    # to a individual torch tensor for compatibility.
-    # All numpy.ndarrays still need to be passed
-    # as Torch Tensors.
-    if isinstance(inputs, torch.Tensor):
-        return inputs
-    if isinstance(inputs, (list, tuple)):
-        # list/tuple of individual values
-        return tuple(
-            [torch.tensor(val) if not isinstance(val, torch.Tensor)  else val for val in inputs]
-        )
-    return torch.tensor(inputs)  # individual value
+from keras_core.backend.torch.core import convert_to_tensor
 
 
 
 def add(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.add(x1, x2)
 
 
 def einsum(subscripts, *operands, **kwargs):
+    operands = [convert_to_tensor(operand) for operand in operands]
     return torch.einsum(subscripts, *operands)
 
 
 def subtract(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.subtract(x1, x2)
 
 
 def matmul(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.matmul(x1, x2)
 
 
 def multiply(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.multiply(x1, x2)
 
 
 def mean(x, axis=None, keepdims=False):
+    x = convert_to_tensor(x)
     return torch.mean(x, axis=axis, keepdims=keepdims)
 
 
 def max(x, axis=None, keepdims=False, initial=None):
+    x = convert_to_tensor(x)
     if axis is None:
         result = torch.max(x)
     else:
+        if isinstance(axis, list):
+            axis = axis[-1]
         result = torch.max(x, dim=axis, keepdim=keepdims)
+
+    if isinstance(getattr(result, "values", None), torch.Tensor):
+        result = result.values
 
     if initial is not None:
         return torch.maximum(
@@ -70,35 +68,44 @@ def absolute(x):
 
 
 def abs(x):
+    x = convert_to_tensor(x)
     return torch.abs(x)
 
 
 def all(x, axis=None, keepdims=False):
-    if axis is None:
-        torch.all(x, dim=axis, keepdim=keepdims)
+    x = convert_to_tensor(x)
+    if axis is not None:
+        if isinstance(axis, list):
+            axis = axis[-1]
+        return torch.all(x, dim=axis, keepdim=keepdims)
     else:
-        torch.all(x)
+        return torch.all(x)
 
 
 def any(x, axis=None, keepdims=False):
-    if axis is None:
-        torch.any(x, dim=axis, keepdim=keepdims)
+    x = convert_to_tensor(x)
+    if axis is not None:
+        if isinstance(axis, list):
+            axis = axis[-1]
+        return torch.any(x, dim=axis, keepdim=keepdims)
     else:
-        torch.any(x)
+        return torch.any(x)
 
 
 def amax(x, axis=None, keepdims=False):
-    if axis is None:
-        torch.amax(x, dim=axis, keepdim=keepdims)
+    x = convert_to_tensor(x)
+    if axis is not None:
+        return torch.amax(x, dim=axis, keepdim=keepdims)
     else:
-        torch.amax(x)
+        return torch.amax(x)
 
 
 def amin(x, axis=None, keepdims=False):
-    if axis is None:
-        torch.amax(x, dim=axis, keepdim=keepdims)
+    x = convert_to_tensor(x)
+    if axis is not None:
+        return torch.amin(x, dim=axis, keepdim=keepdims)
     else:
-        torch.amax(x)
+        return torch.amin(x)
 
 
 def append(
@@ -106,6 +113,7 @@ def append(
     x2,
     axis=None,
 ):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     dim = axis or 0
     return torch.cat((x1, x2), dim=dim)
 
@@ -119,100 +127,133 @@ def arange(start, stop=None, step=None, dtype=None):
 
 
 def arccos(x):
+    x = convert_to_tensor(x)
     return torch.arccos(x)
 
 
 def arcsin(x):
+    x = convert_to_tensor(x)
     return torch.arcsin(x)
 
 
 def arctan(x):
+    x = convert_to_tensor(x)
     return torch.arctan(x)
 
 
 def arctan2(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.arctan2(x1, x2)
 
 
 def argmax(x, axis=None):
+    x = convert_to_tensor(x)
     return torch.argmax(x, dim=axis)
 
 
 def argmin(x, axis=None):
+    x = convert_to_tensor(x)
     return torch.argmin(x, dim=axis)
 
 
 def argsort(x, axis=-1):
+    x = convert_to_tensor(x)
+    if axis is None:
+        axis = -1
+        x = x.reshape(-1)
     return torch.argsort(x, dim=axis)
 
 
 def array(x, dtype=None):
     dtype = to_torch_dtype(dtype)
-    pass
-    # return tfnp.array(x, dtype=dtype)
+    if not isinstance(x, torch.Tensor):
+        return x
+    return x.numpy()
 
 
 def average(x, axis=None, weights=None):
+    x = convert_to_tensor(x)
     if weights is not None:
-        return torch.matmul(x, weights) / torch.sum(weights)
+        weights = convert_to_tensor(weights)
+        return torch.matmul(x, weights.T) / torch.sum(weights)
     return torch.mean(x, axis)
 
 
+def bincount(x, weights=None, minlength=0):
+    x = convert_to_tensor(x)
+    return torch.bincount(x, weights, minlength)
+
+
 def broadcast_to(x, shape):
+    x = convert_to_tensor(x)
     return torch.broadcast_to(x, shape)
 
 
 def ceil(x):
+    x = convert_to_tensor(x)
     return torch.ceil(x)
 
 
 def clip(x, x_min, x_max):
+    x = convert_to_tensor(x)
+    x_min, x_max = convert_to_tensor(x_min), convert_to_tensor(x_max)
     return torch.clip(x, min=x_min, max=x_max)
 
 
 def concatenate(xs, axis=0):
+    xs = [convert_to_tensor(x) for x in xs]
     return torch.cat(xs, dim=axis)
 
 
 def conjugate(x):
+    x = convert_to_tensor(x)
     return conj(x)
 
 
 def conj(x):
+    x = convert_to_tensor(x)
     return torch.conj(x)
 
 
 def copy(x):
+    x = convert_to_tensor(x)
     return torch.clone(x)
 
 
 def cos(x):
+    x = convert_to_tensor(x)
     return torch.cos(x)
 
 
 def count_nonzero(x, axis=None):
+    x = convert_to_tensor(x)
     return torch.count_nonzero(x, dim=axis)
 
 
 def cross(x1, x2, axisa=-1, axisb=-1, axisc=-1, axis=None):
     # There is API divergence between np.cross and torch.cross
     # https://github.com/pytorch/pytorch/issues/50273
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.cross(x1, x2, dim=axis)
 
 
 def cumprod(x, axis=None):
+    x = convert_to_tensor(x)
     return torch.cumprod(x, dim=axis)
 
 
 def cumsum(x, axis=None):
+    x = convert_to_tensor(x)
     return torch.cumsum(x, dim=axis)
 
 
 def diag(x, k=0):
+    x = convert_to_tensor(x)
     return torch.diag(x, diagonal=k)
 
 
 def diagonal(x, offset=0, axis1=0, axis2=1):
+    x = convert_to_tensor(x)
     return torch.diagonal(
        x,
        offset=offset,
@@ -222,6 +263,7 @@ def diagonal(x, offset=0, axis1=0, axis2=1):
 
 
 def dot(x, y):
+    x, y = convert_to_tensor(x), convert_to_tensor(y)
     return torch.dot(x, y)
 
 
@@ -231,26 +273,32 @@ def empty(shape, dtype="float32"):
 
 
 def equal(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.equal(x1, x2)
 
 
 def exp(x):
+    x = convert_to_tensor(x)
     return torch.exp(x)
 
 
 def expand_dims(x, axis):
+    x = convert_to_tensor(x)
     return torch.unsqueeze(x, dim=axis)
 
 
 def expm1(x):
+    x = convert_to_tensor(x)
     return torch.expm1(x)
 
 
 def flip(x, axis=None):
+    x = convert_to_tensor(x)
     return torch.flip(x, dims=axis)
 
 
 def floor(x):
+    x = convert_to_tensor(x)
     return torch.floor(x)
 
 
@@ -265,14 +313,17 @@ def full_like(x, fill_value, dtype=None):
 
 
 def greater(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.greater(x1, x2)
 
 
 def greater_equal(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.greater_equal(x1, x2)
 
 
 def hstack(xs):
+    xs = [convert_to_tensor(x) for x in xs]
     return torch.hstack(xs)
 
 
@@ -283,30 +334,37 @@ def identity(n, dtype="float32"):
 
 
 def imag(x):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.imag(x)
 
 
 def isclose(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.isclose(x1, x2)
 
 
 def isfinite(x):
+    x = convert_to_tensor(x)
     return torch.isfinite(x)
 
 
 def isinf(x):
+    x = convert_to_tensor(x)
     return torch.isinf(x)
 
 
 def isnan(x):
+    x = convert_to_tensor(x)
     return torch.isnan(x)
 
 
 def less(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.less(x1, x2)
 
 
 def less_equal(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.less_equal(x1, x2)
 
 
@@ -319,7 +377,7 @@ def linspace(
         f"Received axis={axis}"
         )
         dtype = to_torch_dtype(dtype)
-    steps = abs(stop - start)/num
+    steps = int(abs(stop - start)/num)
     linspace = torch.linspace(
        start=start,
        end=stop,
@@ -334,34 +392,42 @@ def linspace(
 
 
 def log(x):
+    x = convert_to_tensor(x)
     return torch.log(x)
 
 
 def log10(x):
+    x = convert_to_tensor(x)
     return torch.log10(x)
 
 
 def log1p(x):
+    x = convert_to_tensor(x)
     return torch.log1p(x)
 
 
 def log2(x):
+    x = convert_to_tensor(x)
     return torch.log2(x)
 
 
 def logaddexp(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.logaddexp(x1, x2)
 
 
 def logical_and(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.logical_and(x1, x2)
 
 
 def logical_not(x):
+    x = convert_to_tensor(x)
     return torch.logical_not(x)
 
 
 def logical_or(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.logical_or(x1, x2)
 
 
@@ -372,7 +438,7 @@ def logspace(start, stop, num=50, endpoint=True, base=10, dtype=None, axis=0):
         f"Received axis={axis}"
         )
     dtype = to_torch_dtype(dtype)
-    steps = abs(stop - start)/num
+    steps = int(abs(stop - start)/num)
     logspace = torch.logspace(
        start=start,
        end=stop,
@@ -386,18 +452,27 @@ def logspace(start, stop, num=50, endpoint=True, base=10, dtype=None, axis=0):
 
 
 def maximum(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.maximum(x1, x2)
 
 
 def meshgrid(*x, indexing="xy"):
-    return torch.meshgrid(x, indexing=indexing)
+    x = [convert_to_tensor(sc_tensor) for sc_tensor in x]
+    result = torch.meshgrid(x, indexing=indexing)
+    return [arr.numpy() for arr in result]
 
 
 def min(x, axis=None, keepdims=False, initial=None):
+    x = convert_to_tensor(x)
     if axis is None:
         result = torch.min(x)
     else:
+        if isinstance(axis, list):
+            axis = axis[-1]
         result = torch.min(x, dim=axis, keepdim=keepdims)
+
+    if isinstance(getattr(result, "values", None), torch.Tensor):
+        result = result.values
 
     if initial is not None:
         return torch.minimum(
@@ -407,94 +482,121 @@ def min(x, axis=None, keepdims=False, initial=None):
 
 
 def minimum(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.minimum(x1, x2)
 
 
 def mod(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.remainder(x1, x2)
 
 
 def moveaxis(x, source, destination):
+    x = convert_to_tensor(x)
     return torch.moveaxis(x, source=source, destination=destination)
 
 
 def nan_to_num(x):
+    x = convert_to_tensor(x)
     return torch.nan_to_num(x)
 
 
 def ndim(x):
+    x = convert_to_tensor(x)
     return x.ndim
 
 
 def nonzero(x):
+    x = convert_to_tensor(x)
     return torch.nonzero(x)
 
 
 def not_equal(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.not_equal(x1, x2)
 
 
 def ones_like(x, dtype=None):
+    x = convert_to_tensor(x)
     dtype = to_torch_dtype(dtype)
     return torch.ones_like(x, dtype=dtype)
 
 
 def outer(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.outer(x1, x2)
 
 
 def pad(x, pad_width, mode="constant"):
-    return torch.nn.functional.pad(x, pad=tuple(pad_width), mode=mode)
+    x = convert_to_tensor(x)
+    pad_sum = ()
+    for pad in pad_width:
+        pad_sum += pad
+    return torch.nn.functional.pad(x, pad=pad_sum, mode=mode)
 
 
 def prod(x, axis=None, keepdims=False, dtype=None):
+    x = convert_to_tensor(x)
     dtype = to_torch_dtype(dtype)
     if axis is None:
         return torch.prod(x, dtype=dtype)
+    elif isinstance(axis, list):
+        axis = axis[-1]
     return torch.prod(x, dim=axis, keepdim=keepdims, dtype=dtype)
 
 
 def ravel(x):
+    x = convert_to_tensor(x)
     return torch.ravel(x)
 
 
 def real(x):
+    x = convert_to_tensor(x)
     return torch.real(x)
 
 
 def reciprocal(x):
+    x = convert_to_tensor(x)
     return torch.reciprocal(x)
 
 
 def repeat(x, repeats, axis=None):
+    x = convert_to_tensor(x)
     return torch.repeat_interleave(x, repeats, dim=axis)
 
 
 def reshape(x, new_shape):
+    x = convert_to_tensor(x)
     return torch.reshape(x, new_shape)
 
 
 def roll(x, shift, axis=None):
+    x = convert_to_tensor(x)
     return torch.roll(x, shift, dims=axis)
 
 
 def sign(x):
+    x = convert_to_tensor(x)
     return torch.sign(x)
 
 
 def sin(x):
+    x = convert_to_tensor(x)
     return torch.sin(x)
 
 
 def size(x):
+    x = convert_to_tensor(x)
     return x.size()
 
 
 def sort(x, axis=-1):
+    x = convert_to_tensor(x)
     return torch.sort(x, dim=axis)
 
 
 def split(x, indices_or_sections, axis=0):
+    x = convert_to_tensor(x)
     return torch.split(
         tensor=x,
         split_size_or_sections=indices_or_sections,
@@ -503,42 +605,52 @@ def split(x, indices_or_sections, axis=0):
 
 
 def stack(x, axis=0):
+    x = convert_to_tensor(x)
     return torch.stack(x, dim=axis)
 
 
 def std(x, axis=None, keepdims=False):
+    x = convert_to_tensor(x)
     return torch.std(x, dim=axis, keepdim=keepdims)
 
 
 def swapaxes(x, axis1, axis2):
+    x = convert_to_tensor(x)
     return torch.swapaxes(x, axis0=axis1, axis1=axis2)
 
 
 def take(x, indices, axis=None):
+    x = convert_to_tensor(x)
     return torch.index_select(x, index=indices, dim=axis)
 
 
 def take_along_axis(x, indices, axis=None):
+    x = convert_to_tensor(x)
     return torch.take_along_dim(x, indices, dim=axis)
 
 
 def tan(x):
+    x = convert_to_tensor(x)
     return torch.tan(x)
 
 
 def tensordot(x1, x2, axes=2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.tensordot(x1, x2, dims=axes)
 
 
 def round(x, decimals=0):
+    x = convert_to_tensor(x)
     return torch.round(x, decimals=decimals)
 
 
 def tile(x, repeats):
+    x = convert_to_tensor(x)
     return torch.tile(x, dims=repeats)
 
 
 def trace(x, offset=None, axis1=None, axis2=None):
+    x = convert_to_tensor(x)
     if offset or axis1 or axis2:
         "Arguments not supported by `torch.trace: "
         f""
@@ -552,64 +664,79 @@ def tri(N, M=None, k=0, dtype="float32"):
 
 
 def tril(x, k=0):
+    x = convert_to_tensor(x)
     return torch.tril(x, diagonal=k)
 
 
 def triu(x, k=0):
+    x = convert_to_tensor(x)
     return torch.triu(x, diagonal=k)
 
 
 def vdot(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.vdot(x1, x2)
 
 
 def vstack(xs):
+    x = [convert_to_tensor(x) for x in xs]
     return torch.vstack(xs)
 
 
 def where(condition, x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.where(condition, x1, x2)
 
 
 def divide(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.divide(x1, x2)
 
 
 def true_divide(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.true_divide(x1, x2)
 
 
 def power(x1, x2):
+    x1, x2 = convert_to_tensor(x1), convert_to_tensor(x2)
     return torch.pow(x1, x2)
 
 
 def negative(x):
+    x = convert_to_tensor(x)
     return torch.negative(x)
 
 
 def square(x):
+    x = convert_to_tensor(x)
     return torch.square(x)
 
 
 def sqrt(x):
+    x = convert_to_tensor(x)
     return torch.sqrt(x)
 
 
 def squeeze(x, axis=None):
+    x = convert_to_tensor(x)
     return torch.squeeze(x, dim=axis)
 
 
 def transpose(x, axes=None):
+    x = convert_to_tensor(x)
     if axes is not None:
         return torch.permute(x, dims=axes)
     return torch.t(x)
 
 
 def var(x, axis=None, keepdims=False):
+    x = convert_to_tensor(x)
     return torch.var(x, dim=axis, keepdim=keepdims)
 
 
 def sum(x, axis=None, keepdims=False):
+    x = convert_to_tensor(x)
     if axis is not None:
         return torch.sum(x, axis=axis, keepdim=keepdims)
     return torch.sum(x)
