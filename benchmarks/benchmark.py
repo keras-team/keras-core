434c5388a4d7542833285db7a856e0820d440213
chenmoneygithub <chenmoney@chenmoney-gpu3.us-west1-a.c.keras-team-gcp.internal>
2023-05-31 05:21:57 +0000

    add train benchmark

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -20,7 +20,7 @@ class BenchmarkMetricsCallback:
         if batch == self.end_batch:
             self.state["benchmark_end"] = time.time()
             throughput = (self.end_batch - self.begin_batch) / (
-                self.state["benchmark_end"] - logs["benchmark_begin"]
+                self.state["benchmark_end"] - self.state["benchmark_begin"]
             )
             self.state["throughput"] = throughput
 
@@ -90,7 +90,7 @@ class LayerBenchmark:
     def benchmark_predict(self, num_samples, batch_size, num_iterations=None):
         data_shape = [num_samples] + list(self.input_shape)
         data = np.random.normal(size=data_shape)
-        num_iterations = num_iterations or num_samples // batch_size
+        num_iterations = num_iterations or num_samples // batch_size - 1
         callback = KerasCoreBenchmarkMetricsCallback(num_iterations)
         tf_keras_callback = TFKerasBenchmarkMetricsCallback(num_iterations)
 
@@ -112,6 +112,38 @@ class LayerBenchmark:
         print(
             f"TF Keras throughput: {tf_keras_callback._callback.state['throughput']} samples/sec."
         )
+        
+    def benchmark_train(self, num_samples, batch_size, num_iterations=None):
+        data_shape = [num_samples] + list(self.input_shape)
+        data = np.random.normal(size=data_shape)
+        label = self._keras_core_layer(data)
+        
+        num_iterations = num_iterations or num_samples // batch_size - 1
+        callback = KerasCoreBenchmarkMetricsCallback(num_iterations)
+        tf_keras_callback = TFKerasBenchmarkMetricsCallback(num_iterations)
+
+        self._keras_core_model.compile(loss="mse", optimizer="sgd")
+        self._keras_core_model.fit(
+            data,
+            label,
+            batch_size=batch_size,
+            callbacks=[callback],
+        )
+
+        self._tf_keras_model.compile(loss="mse", optimizer="sgd")
+        self._tf_keras_model.fit(
+            data,
+            label,
+            batch_size=batch_size,
+            callbacks=[tf_keras_callback],
+        )
+
+        print(
+            f"Keras Core throughput: {callback._callback.state['throughput']} samples/sec."
+        )
+        print(
+            f"TF Keras throughput: {tf_keras_callback._callback.state['throughput']} samples/sec."
+        )
 
 
 if __name__ == "__main__":
@@ -120,7 +152,13 @@ if __name__ == "__main__":
     benchmark = LayerBenchmark(layer_name, init_args, input_shape=[32, 32, 3])
 
     benchmark.benchmark_predict(
-        num_samples=100,
-        batch_size=4,
-        num_iterations=20,
+        num_samples=4000,
+        batch_size=20,
+        num_iterations=199,
+    )
+
+    benchmark.benchmark_train(
+        num_samples=4000,
+        batch_size=20,
+        num_iterations=199,
     )
