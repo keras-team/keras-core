90f0f5efed8f5a7ba7a29297d69d61fee206de3c
chenmoneygithub <chenmoney@google.com>
2023-05-30 21:47:11 -0700

    add benchmark class

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -2,8 +2,74 @@ import keras_core
 
 import numpy as np
 import tensorflow as tf
+import time
 
-class LossAndErrorPrintingCallback(keras_core.callbacks.Callback):
+
+class BenchmarkMetricsCallback:
+    def __init__(self, end_batch, begin_batch=2):
+        self.end_batch = end_batch
+        self.begin_batch = begin_batch
+
+        self.state = {}
+
+    def on_train_batch_begin(self, batch, logs=None):
+        if batch == self.begin_batch:
+            self.state["benchmark_begin"] = time.time()
+
+    def on_train_batch_end(self, batch, logs=None):
+        if batch == self.end_batch:
+            self.state["benchmark_end"] = time.time()
+            throughput = (self.end_batch - self.begin_batch) / (
+                self.state["benchmark_end"] - logs["benchmark_begin"]
+            )
+            self.state["throughput"] = throughput
+
+    def on_predict_batch_begin(self, batch, logs=None):
+        if batch == self.begin_batch:
+            self.state["benchmark_begin"] = time.time()
+
+    def on_predict_batch_end(self, batch, logs=None):
+        if batch == self.end_batch:
+            self.state["benchmark_end"] = time.time()
+            throughput = (self.end_batch - self.begin_batch) / (
+                self.state["benchmark_end"] - self.state["benchmark_begin"]
+            )
+            self.state["throughput"] = throughput
+
+
+class KerasCoreBenchmarkMetricsCallback(keras_core.callbacks.Callback):
+    def __init__(self, end_batch, begin_batch=2):
+        self._callback = BenchmarkMetricsCallback(end_batch, begin_batch)
+
+    def on_train_batch_begin(self, batch, logs=None):
+        self._callback.on_train_batch_begin(batch, logs)
+
+    def on_train_batch_end(self, batch, logs=None):
+        self._callback.on_train_batch_end(batch, logs)
+
+    def on_predict_batch_begin(self, batch, logs=None):
+        self._callback.on_predict_batch_begin(batch, logs)
+
+    def on_predict_batch_end(self, batch, logs=None):
+        self._callback.on_predict_batch_end(batch, logs)
+
+
+class TFKerasBenchmarkMetricsCallback(tf.keras.callbacks.Callback):
+    def __init__(self, end_batch, begin_batch=2):
+        self._callback = BenchmarkMetricsCallback(end_batch, begin_batch)
+
+    def on_train_batch_begin(self, batch, logs=None):
+        self._callback.on_train_batch_begin(batch, logs)
+
+    def on_train_batch_end(self, batch, logs=None):
+        self._callback.on_train_batch_end(batch, logs)
+
+    def on_predict_batch_begin(self, batch, logs=None):
+        self._callback.on_predict_batch_begin(batch, logs)
+
+    def on_predict_batch_end(self, batch, logs=None):
+        self._callback.on_predict_batch_end(batch, logs)
+
 
 class LayerBenchmark:
     def __init__(self, layer_name, init_args, input_shape, num_iterations=100):
@@ -20,20 +86,41 @@ class LayerBenchmark:
 
         self.input_shape = input_shape
         self.num_iterations = num_iterations
-        
 
-    def benchmark_predict(self, num_samples, num_iterations):
+    def benchmark_predict(self, num_samples, batch_size, num_iterations=None):
         data_shape = [num_samples] + list(self.input_shape)
-        data = np.random.normal(data_shape)
-                
-        
+        data = np.random.normal(size=data_shape)
+        num_iterations = num_iterations or num_samples // batch_size
+        callback = KerasCoreBenchmarkMetricsCallback(num_iterations)
+        tf_keras_callback = TFKerasBenchmarkMetricsCallback(num_iterations)
 
-    def run(self):
-        inputs = np.random.normal(size=self.input_shape)
-        self.model.build(input_shape=self.input_shape)
-        self.model.summary()
-        self.model(inputs)
-        layer = self.model.layers[-1]
-        self.benchmark_predict(layer, inputs)
+        self._keras_core_model.predict(
+            data,
+            batch_size=batch_size,
+            callbacks=[callback],
+        )
 
-def benchmark_predict(self, layer, inputs)
+        self._tf_keras_model.predict(
+            data,
+            batch_size=batch_size,
+            callbacks=[tf_keras_callback],
+        )
+
+        print(
+            f"Keras Core throughput: {callback._callback.state['throughput']} samples/sec."
+        )
+        print(
+            f"TF Keras throughput: {tf_keras_callback._callback.state['throughput']} samples/sec."
+        )
+
+
+if __name__ == "__main__":
+    layer_name = "BatchNormalization"
+    init_args = {}
+    benchmark = LayerBenchmark(layer_name, init_args, input_shape=[32, 32, 3])
+
+    benchmark.benchmark_predict(
+        num_samples=100,
+        batch_size=4,
+        num_iterations=20,
+    )
