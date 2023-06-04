7d572a18d40eb887468db27595e8a8bc92eb959a
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2023-06-04 22:58:41 +0000

    format the code

--- keras_core/backend/torch/core.py
+++ keras_core/backend/torch/core.py
@@ -47,7 +47,7 @@ class Variable(KerasVariable):
 
     def _convert_to_tensor(self, value, dtype=None):
         return convert_to_tensor(value, dtype=dtype)
-    
+
     def numpy(self):
         return self.value.detach().numpy()
 
