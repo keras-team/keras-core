e2975615b0f792fac67831dd94d9aeb68e5b7636
Neel Kovelamudi <nkovela@google.com>
2023-06-08 19:04:05 +0000

    Place all funtions in __main__

--- examples/gpu_demo_torch.py
+++ examples/gpu_demo_torch.py
@@ -18,32 +18,41 @@ learning_rate = 0.01
 batch_size = 128
 num_epochs = 1
 
-# Load the data and split it between train and test sets
-(x_train, y_train), (x_test, y_test) = keras_core.datasets.mnist.load_data()
+def get_data():
+    # Load the data and split it between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras_core.datasets.mnist.load_data()
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
 
-# Create the Keras model
-model = keras_core.Sequential(
-    [
-        layers.Input(shape=(28, 28, 1)),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes),
-    ]
-)
+    # Create a TensorDataset
+    dataset = torch.utils.data.TensorDataset(
+        torch.from_numpy(x_train), torch.from_numpy(y_train)
+    )
+    return dataset
+
+def get_model():
+    # Create the Keras model
+    model = keras_core.Sequential(
+        [
+            layers.Input(shape=(28, 28, 1)),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(num_classes),
+        ]
+    )
+    return model
 
 #################################################################
 ######## Writing a torch training loop for a Keras model ########
@@ -86,12 +95,7 @@ def setup(rank, world_size):
     torch.cuda.set_device(device)
 
 
-def prepare(rank, world_size, batch_size):
-    # Create a TensorDataset
-    dataset = torch.utils.data.TensorDataset(
-        torch.from_numpy(x_train), torch.from_numpy(y_train)
-    )
-
+def prepare(dataset, rank, world_size, batch_size):
     sampler = DistributedSampler(
         dataset,
         num_replicas=world_size,
@@ -114,12 +118,15 @@ def cleanup():
     dist.destroy_process_group()
 
 
-def master(rank, world_size):
+def main(rank, world_size):
     # setup the process groups
     setup(rank, world_size)
 
+    dataset = get_data()
+    model = get_model()
+
     # prepare the dataloader
-    dataloader = prepare(rank, world_size, batch_size)
+    dataloader = prepare(dataset, rank, world_size, batch_size)
 
     # Instantiate the torch optimizer
     print("Num params:", len(list(model.parameters())))
@@ -135,16 +142,6 @@ def master(rank, world_size):
     train(ddp_model, train_loader, num_epochs, optimizer, loss_fn)
 
     cleanup()
-
-
-# GPU parameters
-world_size = torch.cuda.device_count()
-
-torch.multiprocessing.spawn(
-    master,
-    args=(world_size),
-    nprocs=world_size
-)
 
 
 ################################################################
@@ -182,3 +179,12 @@ class MyModel(nn.Module):
 
 # train(torch_module, train_loader, num_epochs, optimizer, loss_fn)
 
+if __name__ == "__main__":
+    # GPU parameters
+    world_size = torch.cuda.device_count()
+
+    torch.multiprocessing.spawn(
+        main,
+        args=(world_size),
+        nprocs=world_size
+    )
