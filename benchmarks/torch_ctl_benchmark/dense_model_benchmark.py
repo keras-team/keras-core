ce42fab4e70818b478582e82453ad34997bbf63f
chenmoneygithub <chenmoney@google.com>
2023-06-07 17:14:38 -0700

    refactor the code

--- benchmarks/torch_ctl_benchmark/dense_model_benchmark.py
+++ benchmarks/torch_ctl_benchmark/dense_model_benchmark.py
@@ -1,3 +1,9 @@
+""" Benchmark Keras performance with torch custom training loop.
+
+In this file we use a model with 3 dense layers. Training loop is written in the
+vanilla torch way, and we compare the performance between building model with
+Keras and torch.
+"""
 import time
 
 import numpy as np
@@ -9,8 +15,8 @@ import keras_core
 from keras_core import layers
 
 num_classes = 2
-input_shape = (3, 256, 256)
-batch_size = 128
+input_shape = (8192,)
+batch_size = 4096
 num_batches = 20
 
 x_train = np.random.normal(
@@ -32,28 +38,35 @@ class TorchModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv = torch.nn.Conv2d(3, 32, kernel_size=(3, 3))
-        self.activation = torch.nn.ReLU()
-        self.max_pool = torch.nn.MaxPool2d((2, 2))
-        self.flatten = torch.nn.Flatten()
-        self.dense = torch.nn.LazyLinear(num_classes)
+        self.dense1 = torch.nn.Linear(8192, 64)
+        self.activation1 = torch.nn.ReLU()
+        self.dense2 = torch.nn.Linear(64, 8)
+        self.activation2 = torch.nn.ReLU()
+        self.dense3 = torch.nn.Linear(8, num_classes)
         self.softmax = torch.nn.Softmax(dim=1)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.activation(x)
-        x = self.max_pool(x)
-        x = self.flatten(x)
-        x = self.dense(x)
+        x = self.dense1(x)
+        x = self.activation1(x)
+        x = self.dense2(x)
+        x = self.activation2(x)
+        x = self.dense3(x)
         x = self.softmax(x)
         return x
 
 
 def train(model, train_loader, num_epochs, optimizer, loss_fn, framework):
-    start = time.time()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    start = None
+    average_batch_time_per_epoch = []
     for epoch in range(num_epochs):
         running_loss = 0.0
         for batch_idx, (inputs, targets) in enumerate(train_loader):
+            if batch_idx == 1:
+                start = time.time()
+            inputs = inputs.to(device)
+            targets = targets.to(device)
             # Forward pass
             outputs = model(inputs)
             loss = loss_fn(outputs, targets)
@@ -73,18 +86,19 @@ def train(model, train_loader, num_epochs, optimizer, loss_fn, framework):
                     f"Loss: {running_loss / 10}"
                 )
                 running_loss = 0.0
+        end = time.time()
+        average_batch_time_per_epoch.append((end - start) / (num_batches - 1))
+    average_time = np.mean(average_batch_time_per_epoch)
 
-    end = time.time()
-    print(f"Total time in {framework}: {end - start}")
+    print(f"Time per batch in {framework}: {average_time:.2f}")
 
 
 def run_keras_core_custom_training_loop():
     keras_model = keras_core.Sequential(
         [
             layers.Input(shape=input_shape),
-            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-            layers.MaxPooling2D(pool_size=(2, 2)),
-            layers.Flatten(),
+            layers.Dense(64, activation="relu"),
+            layers.Dense(8, activation="relu"),
             layers.Dense(num_classes),
             layers.Softmax(),
         ]
@@ -115,5 +129,6 @@ def run_torch_custom_training_loop():
     )
 
 
-run_keras_core_custom_training_loop()
-run_torch_custom_training_loop()
+if __name__ == "__main__":
+    run_keras_core_custom_training_loop()
+    run_torch_custom_training_loop()
