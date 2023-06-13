191805af52cc69a9ead6ca3f591f56ab03eb65e4
Chen Qian <qianchen94era@gmail.com>
2023-06-13 12:52:11 -0700

    fix comments

--- keras_core/backend/torch/numpy.py
+++ keras_core/backend/torch/numpy.py
@@ -39,6 +39,8 @@ def multiply(x1, x2):
 
 
 def mean(x, axis=None, keepdims=False):
+    if isinstance(x, list):
+        x = stack(x)
     x = convert_to_tensor(x)
     # Conversion to float necessary for `torch.mean`
     x = cast(x, "float32") if x.dtype in TORCH_INT_TYPES else x
