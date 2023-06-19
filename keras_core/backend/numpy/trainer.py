7143f06d9eab6fa46caa6a6fc47f73664f2ea719
ariG23498 <aritra.born2fly@gmail.com>
2023-06-19 08:36:19 +0000

    chore: adding necessaity methods to the numpy trainer

--- keras_core/backend/numpy/trainer.py
+++ keras_core/backend/numpy/trainer.py
@@ -1,3 +1,21 @@
 class NumpyTrainer:
     def __init__(self):
         raise NotImplementedError("Numpy Backend should not have a trainer")
+
+    def fit(self):
+        raise NotImplementedError("Numpy Backend should not have a trainer")
+
+    def predict(self):
+        raise NotImplementedError("Numpy Backend should not have a trainer")
+
+    def evaluate(self):
+        raise NotImplementedError("Numpy Backend should not have a trainer")
+
+    def train_on_batch(self):
+        raise NotImplementedError("Numpy Backend should not have a trainer")
+
+    def test_on_batch(self):
+        raise NotImplementedError("Numpy Backend should not have a trainer")
+
+    def predict_on_batch(self):
+        raise NotImplementedError("Numpy Backend should not have a trainer")
