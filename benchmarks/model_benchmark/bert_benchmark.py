8857477839eb39085d9d4179392f8e6f8f3a6b9c
chenmoneygithub <chenmoney@chenmoney-gpu-scratch.us-west1-a.c.keras-team-gcp.internal>
2023-06-23 23:03:25 +0000

    rebase main

--- benchmarks/model_benchmark/bert_benchmark.py
+++ benchmarks/model_benchmark/bert_benchmark.py
@@ -4,8 +4,8 @@ To run the script, make sure you are in benchmarks/ directory, abd run the
 command below:
 ```
 python3 -m model_benchmark.bert_benchmark \
-    --epochs 5 \
-    --batch_size 16
+    --epochs 2 \
+    --batch_size 32
 ```
 
 """
@@ -21,6 +21,7 @@ from absl import app
 from absl import flags
 from absl import logging
 
+from model_benchmark.benchmark_utils import BenchmarkMetricsCallback
 
 
 flags.DEFINE_string("model_size", "small", "The size of model to benchmark.")
@@ -42,28 +43,6 @@ MODEL_SIZE_MAP = {
     "base": "bert_base_en_uncased",
     "large": "bert_large_en_uncased",
 }
-
-
-class BenchmarkMetricsCallback(keras.callbacks.Callback):
-    def __init__(self, start_batch=1, stop_batch=None):
-        self.start_batch = start_batch
-        self.stop_batch = stop_batch
-
-        # Store the throughput of each epoch.
-        self.state = {"throughput": []}
-
-    def on_train_batch_begin(self, batch, logs=None):
-        if batch == self.start_batch:
-            self.state["epoch_begin_time"] = time.time()
-
-    def on_train_batch_end(self, batch, logs=None):
-        if batch == self.stop_batch:
-            epoch_end_time = time.time()
-            throughput = (self.stop_batch - self.start_batch + 1) / (
-                epoch_end_time - self.state["epoch_begin_time"]
-            )
-            self.state["throughput"].append(throughput)
-
 
 def load_data():
     """Load data.
