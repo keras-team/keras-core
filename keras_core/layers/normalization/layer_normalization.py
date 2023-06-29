a3673c8aec862b60b517fe7c0f89e79a4e2e5e27
Chen Qian <qianchen94era@gmail.com>
2023-06-28 17:43:15 -0700

    fix test

--- keras_core/layers/normalization/layer_normalization.py
+++ keras_core/layers/normalization/layer_normalization.py
@@ -201,15 +201,16 @@ class LayerNormalization(Layer):
         variance = ops.var(inputs, axis=self.axis, keepdims=True)
 
         scale, offset = _broadcast(self.gamma), _broadcast(self.beta)
-        scale = ops.cast(scale, inputs.dtype)
-        offset = ops.cast(offset, inputs.dtype)
 
         # Compute the batch normalization.
         inv = 1 / ops.sqrt(variance + self.epsilon)
         if scale is not None:
+            scale = ops.cast(scale, inputs.dtype)
             inv = inv * scale
-
-        x = offset - mean * inv if offset is not None else -mean * inv
+        x = -mean * inv
+        if offset is not None:
+            offset = ops.cast(offset, inputs.dtype)
+            x = offset + x
         outputs = inputs * ops.cast(inv, inputs.dtype) + ops.cast(
             x, inputs.dtype
         )
