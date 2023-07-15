52d7d81523435d9996f235b1f47db1bb9fbeb7dd
Sheiphan <98021966+Sheiphan@users.noreply.github.com>
2023-07-16 03:38:30 +0530

    Update math.py

--- keras_core/ops/math.py
+++ keras_core/ops/math.py
@@ -39,24 +39,24 @@ def segment_sum(data, segment_ids, num_segments=None, sorted=False):
 
     Args:
         data: Input tensor.
-        segment_ids: A 1-D tensor containing segment indices for each 
-                element in `data`.
-        num_segments: An integer representing the total number of 
+        segment_ids: A 1-D tensor containing segment indices for each
+            element in `data`.
+        num_segments: An integer representing the total number of
             segments. If not specified, it is inferred from the maximum
             value in `segment_ids`.
-        sorted: A boolean indicating whether `segment_ids` is sorted. 
-                Default is `False`.
+        sorted: A boolean indicating whether `segment_ids` is sorted.
+            Default is `False`.
 
     Returns:
-        A tensor containing the sum of segments, where each element 
+        A tensor containing the sum of segments, where each element
         represents the sum of the corresponding segment in `data`.
 
     Example:
-    ```
+    ```python
     >>> data = keras_core.ops.convert_to_tensor([1, 2, 3, 4, 5, 6])
     >>> segment_ids = keras_core.ops.convert_to_tensor([0, 1, 0, 1, 0, 1])
     >>> segment_sum(data, segment_ids)
-    ([9 12], shape=(2,), dtype=int32)
+    array([9 12], shape=(2,), dtype=int32)
     ```
     """
     if any_symbolic_tensors((data,)):
@@ -92,22 +92,22 @@ def top_k(x, k, sorted=True):
     Args:
         x: Input tensor.
         k: An integer representing the number of top elements to retrieve.
-        sorted: A boolean indicating whether to sort the output in 
-            descending order.Default is `True`.
+        sorted: A boolean indicating whether to sort the output in
+        descending order.Default is `True`.
 
     Returns:
-        A tuple containing two tensors. The first tensor contains the 
-        top-k values, and the second tensor contains the indices of the 
+        A tuple containing two tensors. The first tensor contains the
+        top-k values, and the second tensor contains the indices of the
         top-k values in the input tensor.
 
     Example:
-    ```
+    ```python
     >>> x = keras_core.ops.convert_to_tensor([5, 2, 7, 1, 9, 3])
     >>> values, indices = top_k(x, k=3)
     >>> print(values)
-    ([9 7 5], shape=(3,), dtype=int32)
+    array([9 7 5], shape=(3,), dtype=int32)
     >>> print(indices)
-    ([4 2 0], shape=(3,), dtype=int32)
+    array([4 2 0], shape=(3,), dtype=int32)
     ```
     """
     if any_symbolic_tensors((x,)):
@@ -137,17 +137,18 @@ def in_top_k(targets, predictions, k):
         k: An integer representing the number of predictions to consider.
 
     Returns:
-        A boolean tensor of the same shape as `targets`, where each element 
+        A boolean tensor of the same shape as `targets`, where each element
         indicates whether the corresponding target is in the top-k predictions.
         
     Example:
-    ```
+    ```python
     >>> targets = keras_core.ops.convert_to_tensor([2, 5, 3])
-    >>> predictions = keras_core.ops.convert_to_tensor([[0.1, 0.4, 0.6, 0.9, 0.5],
-                                                        [0.1, 0.7, 0.9, 0.8, 0.3],
-                                                        [0.1, 0.6, 0.9, 0.9, 0.5]])
+    >>> predictions = keras_core.ops.convert_to_tensor(
+                                                    [[0.1, 0.4, 0.6, 0.9, 0.5],
+                                                    [0.1, 0.7, 0.9, 0.8, 0.3],
+                                                    [0.1, 0.6, 0.9, 0.9, 0.5]])
     >>> in_top_k(targets, predictions, k=3)
-    ([ True False  True], shape=(3,), dtype=bool)
+    array([ True False  True], shape=(3,), dtype=bool)
     ```
     """
     if any_symbolic_tensors((targets, predictions)):
@@ -171,24 +172,25 @@ class Logsumexp(Operation):
 
 @keras_core_export("keras_core.ops.logsumexp")
 def logsumexp(x, axis=None, keepdims=False):
-    """Computes the logarithm of the sum of exponentials of elements in a tensor.
+    """Computes the logarithm of sum of exponentials of elements in a tensor.
 
     Args:
         x: Input tensor.
-        axis: An integer or a tuple of integers specifying the axis/axes along 
-            which to compute the sum. If `None`, the sum is computed over all elements. 
-            Default is `None`.
-        keepdims: A boolean indicating whether to keep the dimensions of the input 
-            tensor when computing the sum. Default is False.
+        axis: An integer or a tuple of integers specifying the axis/axes
+            along which to compute the sum. If `None`, the sum is computed
+            over all elements. Default is `None`.
+        keepdims: A boolean indicating whether to keep the dimensions of
+            the input tensor when computing the sum. Default is False.
 
     Returns:
-        A tensor containing the logarithm of the sum of exponentials of elements in `x`.
+        A tensor containing the logarithm of the sum of exponentials of
+        elements in `x`.
 
     Example:
-    ```
+    ```python
     >>> x = keras_core.ops.convert_to_tensor([1., 2., 3.])
     >>> logsumexp(x)
-    (3.407606, shape=(), dtype=float32)
+    array(3.407606, shape=(), dtype=float32)
     ```
     """
     if any_symbolic_tensors((x,)):
@@ -249,16 +251,16 @@ def qr(x, mode="reduced"):
             - 'complete': Returns the complete QR decomposition.
 
     Returns:
-        A tuple containing two tensors. The first tensor represents the 
-        orthogonal matrix Q, and the second tensor represents the upper 
+        A tuple containing two tensors. The first tensor represents the
+        orthogonal matrix Q, and the second tensor represents the upper
         triangular matrix R.
 
     Example:
-    ```
+    ```python
     >>> x = keras_core.ops.convert_to_tensor([[1., 2.], [3., 4.], [5., 6.]])
     >>> q, r = qr(x)
     >>> print(q)
-    ([[-0.16903079  0.897085  ]
+    array([[-0.16903079  0.897085]
      [-0.5070925   0.2760267 ]
      [-0.8451542  -0.34503305]], shape=(3, 2), dtype=float32)
     ```
