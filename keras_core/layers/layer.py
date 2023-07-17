19b0e397538ae28f55ce8e7bcb398a77a71e1631
Neel Kovelamudi <nkovela@google.com>
2023-07-17 23:05:52 +0000

    Add TF tracking attribute

--- keras_core/layers/layer.py
+++ keras_core/layers/layer.py
@@ -293,11 +293,14 @@ class Layer(BackendLayer, Operation):
                 ),
             }
         )
+        _self_setattr_tracking = getattr(self, "_self_setattr_tracking", True)
+        self._self_setattr_tracking = False
         self._trainable_variables = trainable_variables
         self._non_trainable_variables = non_trainable_variables
         self._layers = layers
         self._metrics = metrics
         self._seed_generators = seed_generators
+        self._self_setattr_tracking = _self_setattr_tracking
 
     @property
     def input_spec(self):
