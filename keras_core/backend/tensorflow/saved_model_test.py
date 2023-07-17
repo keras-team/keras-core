2cbb158da517df972554927440d0573e082686b7
Neel Kovelamudi <nkovela@google.com>
2023-07-17 23:52:18 +0000

    Add saved_model_test

---
+++ keras_core/backend/tensorflow/saved_model_test.py
@@ -0,0 +1,80 @@
+"""Tests for tf.distribute related functionality under tf implementation."""
+
+import os
+import numpy as np
+import pytest
+import tensorflow as tf
+from tensorflow.python.eager import context
+
+from keras_core import backend
+from keras_core import layers
+from keras_core import models
+from keras_core import metrics
+from keras_core import testing
+from keras_core.backend.tensorflow import trainer as tf_trainer
+from keras_core.saving import object_registration
+
+
+@object_registration.register_keras_serializable(package="my_custom_package")
+class CustomModelX(models.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dense1 = layers.Dense(1)
+        self.dense2 = layers.Dense(1)
+
+    def call(self, inputs):
+        out = self.dense1(inputs)
+        return self.dense2(out)
+
+    def one(self):
+        return 1
+
+
+# @pytest.mark.skipif(
+#     backend.backend() != "tensorflow",
+#     reason="The SavedModel test can only run with TF backend.",
+# )
+class SavedModelTest(testing.TestCase):
+    def test_sequential(self):
+        model = models.Sequential([
+            layers.Dense(1)
+        ])
+        model.compile(loss="mse", optimizer="adam")
+        X_train = np.random.rand(100, 3)
+        y_train = np.random.rand(100, 1)
+        model.fit(X_train, y_train)
+        path = os.path.join(self.get_temp_dir(), "my_keras_core_model")
+        tf.saved_model.save(model, path)
+        restored_model = load(path)
+        self.assertAllClose(model(X_train), restored_model(X_train), rtol=1e-4, atol=1e-4)
+
+    def test_functional(self):
+        inputs = layers.Input(shape=(3,))
+        x = layers.Dense(1, name="first_dense")(inputs)
+        outputs = layers.Dense(1, name="second_dense")(x)
+        model = models.Model(inputs, outputs)
+        model.compile(
+            optimizer="adam", loss="mse",
+        )
+        X_train = np.random.rand(100, 3)
+        y_train = np.random.rand(100, 1)
+        model.fit(X_train, y_train)
+        path = os.path.join(self.get_temp_dir(), "my_keras_core_model")
+        tf.saved_model.save(model, path)
+        restored_model = load(path)
+        self.assertAllClose(model(X_train), restored_model(X_train), rtol=1e-4, atol=1e-4)
+
+    def test_subclassed(self):
+        model = CustomModelX()
+        model.compile(
+                optimizer="adam",
+                loss="mse",
+                metrics=[metrics.Hinge(), "mse"],
+            )
+        X_train = np.random.rand(100, 3)
+        y_train = np.random.rand(100, 1)
+        model.fit(X_train, y_train)
+        path = os.path.join(self.get_temp_dir(), "my_keras_core_model")
+        tf.saved_model.save(model, path)
+        restored_model = load(path)
+        self.assertAllClose(model(X_train), restored_model(X_train), rtol=1e-4, atol=1e-4)
