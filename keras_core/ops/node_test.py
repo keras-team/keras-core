fd0c83d82153cdb468a197fdf8bf8997629c73f2
Adithya Kamath <kamathis4@gmail.com>
2023-07-18 23:42:45 +0530

    Node test

--- keras_core/ops/node_test.py
+++ keras_core/ops/node_test.py
@@ -1,19 +1,17 @@
-from keras.src.engine import base_layer
-
 from keras_core import testing
 from keras_core.backend import KerasTensor
+from keras_core import ops, Layer
+from keras_core.ops.node import Node
 
 
-class DummyLayer(base_layer.Layer):
+class DummyLayer(Layer):
     pass
 
 
 class NodeTest(testing.TestCase):
-
-    def test_happy_path(self):
-        a = KerasTensor(shape=(2, 3, 5))
-        b = KerasTensor(shape=(2, 3, 5))
-
+    def test_something(self):
+        a = KerasTensor(shape=(2, 3, 4))
         a_layer = DummyLayer()
-        # node = Node(a_layer, outputs=a)
-        # self.assertEqual(node.output_tensors, a)
+        node = Node(a_layer, outputs=a, call_args=(), call_kwargs={})
+        self.assertEqual(node.output_tensors[0], a)
+        self.assertEqual(node.is_input, True)
