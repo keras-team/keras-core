9fec9fcc69a7a72fdfbe7eae17e9defba020b4c5
oxytracker <87067648+oxytracker@users.noreply.github.com>
2023-07-20 09:07:19 +0530

    Command and kwargs added

--- keras_core/ops/node_test.py
+++ keras_core/ops/node_test.py
@@ -11,6 +11,7 @@ class DummyLayer(Layer):
 
 
 class NodeTest(testing.TestCase):
+    # Testing a simple node and layer combination **a**
     def test_simple_case(self):
         shape = (2, 3, 4)
         a = KerasTensor(shape=shape)
@@ -22,6 +23,7 @@ class NodeTest(testing.TestCase):
         self.assertEqual(node.output_tensors[0], a)
         self.assertEqual(node.output_tensors[0].shape, shape)
 
+    # Testing a simple node connection with args and kwargs **a** --> **b**
     def test_single_wired_layers(self):
         shape = (2, 3, 4)
         a = KerasTensor(shape=shape)
@@ -29,8 +31,11 @@ class NodeTest(testing.TestCase):
         node1 = Node(a_layer, outputs=a, call_args=(), call_kwargs={})
 
         b = KerasTensor(shape=shape)
+        x = KerasTensor(shape=shape)
+        kwargs = {"x": x}
+        args = (a,)
         b_layer = DummyLayer()
-        node2 = Node(b_layer, outputs=b, call_args=(a,), call_kwargs={})
+        node2 = Node(b_layer, outputs=b, call_args=args, call_kwargs=kwargs)
 
         self.assertEqual(node1.is_input, True)
         self.assertEqual(node2.is_input, False)
@@ -41,16 +46,21 @@ class NodeTest(testing.TestCase):
         self.assertEqual(node1.output_tensors[0], a)
         self.assertEqual(node1.output_tensors[0].shape, shape)
 
-        self.assertEqual(a_layer.__dict__["_inbound_nodes"][0], node1)
-        self.assertEqual(a_layer.__dict__["_outbound_nodes"][0], node2)
+        self.assertEqual(a_layer._inbound_nodes[0], node1)
+        self.assertEqual(a_layer._outbound_nodes[0], node2)
 
-        self.assertEqual(b_layer.__dict__["_inbound_nodes"][0], node2)
+        self.assertEqual(b_layer._inbound_nodes[0], node2)
         self.assertEqual(node2.parent_nodes[0], node1)
 
+        self.assertEqual(node2.input_tensors, [a, x])
+        self.assertEqual(node2.arguments.kwargs, kwargs)
+        self.assertEqual(node2.arguments.args, args)
+
+    # Testing when output tensor is not Keras Tensor
     def test_output_tensor_error(self):
         a = np.random.rand(2, 3, 4)
         a_layer = DummyLayer()
         with self.assertRaisesRegex(
-            ValueError, "operation outputs must be tensors."
+                ValueError, "operation outputs must be tensors."
         ):
             Node(a_layer, outputs=a, call_args=(), call_kwargs={})
