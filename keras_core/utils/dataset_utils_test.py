6eec8cadc9672cbd56b1c9bc15743defd2fe0b39
asingh9530 <abhinav199530singh@gmail.com>
2023-07-20 19:31:04 +0530

    added unittest using cardinality

--- keras_core/utils/dataset_utils_test.py
+++ keras_core/utils/dataset_utils_test.py
@@ -9,58 +9,58 @@ class DatasetUtilsTest(test_case.TestCase):
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = [np.random.sample((n_sample,n_cols)), np.random.sample((n_sample,n_pred))]
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = [np.random.sample((n_sample, 100, n_cols)), np.random.sample((n_sample,n_pred))]
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (100, n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = [np.random.sample((n_sample,10, 10, n_cols)), np.random.sample((n_sample,n_pred))]
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (10, 10, n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = [np.random.sample((n_sample, 100, 10, 30, n_cols)), np.random.sample((n_sample,n_pred))]
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (100, 10, 30, n_cols))
 
     def test_split_dataset_tuple(self):
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = (np.random.sample((n_sample,n_cols)), np.random.sample((n_sample,n_pred)))
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = (np.random.sample((n_sample, 100, n_cols)), np.random.sample((n_sample,n_pred)))
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (100, n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = (np.random.sample((n_sample,10, 10, n_cols)), np.random.sample((n_sample,n_pred)))
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (10, 10, n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         dataset = (np.random.sample((n_sample, 100, 10, 30, n_cols)), np.random.sample((n_sample,n_pred)))
         dataset_left, dataset_right = split_dataset(dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (100, 10, 30, n_cols))
 
     def test_split_dataset_tensorflow(self):
@@ -68,32 +68,32 @@ class DatasetUtilsTest(test_case.TestCase):
         features, labels  = (np.random.sample((n_sample,n_cols)), np.random.sample((n_sample,n_pred)))
         tf_dataset = tf.data.Dataset.from_tensor_slices((features,labels))
         dataset_left, dataset_right = split_dataset(tf_dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         features, labels = (np.random.sample((n_sample, 100, n_cols)), np.random.sample((n_sample,n_pred)))
         tf_dataset = tf.data.Dataset.from_tensor_slices((features,labels))
         dataset_left, dataset_right = split_dataset(tf_dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (100, n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         features, labels = (np.random.sample((n_sample,10, 10, n_cols)), np.random.sample((n_sample,n_pred)))
         tf_dataset = tf.data.Dataset.from_tensor_slices((features,labels))
         dataset_left, dataset_right = split_dataset(tf_dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (10, 10, n_cols))
 
         n_sample, n_cols, n_pred, left_size, right_size = 100, 2, 1, 0.2, 0.8
         features, labels = (np.random.sample((n_sample, 100, 10, 30, n_cols)), np.random.sample((n_sample,n_pred)))
         tf_dataset = tf.data.Dataset.from_tensor_slices((features,labels))
         dataset_left, dataset_right = split_dataset(tf_dataset, left_size=left_size, right_size=right_size)
-        self.assertEqual(len([sample for sample in dataset_left]), int(n_sample * left_size))
-        self.assertEqual(len([sample for sample in dataset_right]), int(n_sample * right_size))
+        self.assertEqual(int(dataset_left.cardinality()), int(n_sample * left_size))
+        self.assertEqual(int(dataset_right.cardinality()), int(n_sample * right_size))
         self.assertEqual([sample for sample in dataset_right][0][0].shape, (100, 10, 30, n_cols))
 
     def test_split_dataset_torch(self):
