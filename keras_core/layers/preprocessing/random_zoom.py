9f23afd513b26b25bb975508c18fa70be8143734
HongYu <20734616+james77777778@users.noreply.github.com>
2023-07-26 01:48:26 +0000

    Update docstring

--- keras_core/layers/preprocessing/random_zoom.py
+++ keras_core/layers/preprocessing/random_zoom.py
@@ -30,42 +30,41 @@ class RandomZoom(TFDataLayer):
     (independently of which backend you're using).
 
     Args:
-        height_factor: a float represented as fraction of value,
-            or a tuple of size 2 representing lower and upper bound
-            for zooming vertically. When represented as a single float,
-            this value is used for both the upper and
-            lower bound. A positive value means zooming out,
-            while a negative value
-            means zooming in. For instance, `height_factor=(0.2, 0.3)`
-            result in an output zoomed out by a random amount
-            in the range `[+20%, +30%]`.
-            `height_factor=(-0.3, -0.2)` result in an output zoomed
-            in by a random amount in the range `[+20%, +30%]`.
-        width_factor: a float represented as fraction of value,
-            or a tuple of size 2 representing lower and upper bound
-            for zooming horizontally. When
-            represented as a single float, this value is used
-            for both the upper and
-            lower bound. For instance, `width_factor=(0.2, 0.3)`
-            result in an output
-            zooming out between 20% to 30%.
-            `width_factor=(-0.3, -0.2)` result in an
-            output zooming in between 20% to 30%. `None` means
-            i.e., zooming vertical and horizontal directions
-            by preserving the aspect ratio. Defaults to `None`.
-        fill_mode: Points outside the boundaries of the input are
-            filled according to the given mode
-            (one of `{"constant", "reflect", "wrap", "nearest"}`).
-            - *reflect*: `(d c b a | a b c d | d c b a)`
-                The input is extended by reflecting about
-                the edge of the last pixel.
-            - *constant*: `(k k k k | a b c d | k k k k)`
+        height_factor: a float represented as fraction of value, or a tuple of
+            size 2 representing lower and upper bound for zooming vertically.
+            When represented as a single float, this value is used for both the
+            upper and lower bound. A positive value means zooming out, while a
+            negative value means zooming in. For instance,
+            `height_factor=(0.2, 0.3)` result in an output zoomed out by a
+            random amount in the range `[+20%, +30%]`.
+            `height_factor=(-0.3, -0.2)` result in an output zoomed in by a
+            random amount in the range `[+20%, +30%]`.
+        width_factor: a float represented as fraction of value, or a tuple of
+            size 2 representing lower and upper bound for zooming horizontally.
+            When represented as a single float, this value is used for both the
+            upper and lower bound. For instance, `width_factor=(0.2, 0.3)`
+            result in an output zooming out between 20% to 30%.
+            `width_factor=(-0.3, -0.2)` result in an output zooming in between
+            20% to 30%. `None` means i.e., zooming vertical and horizontal
+            directions by preserving the aspect ratio. Defaults to `None`.
+        fill_mode: Points outside the boundaries of the input are filled
+            according to the given mode. Available methods are `"constant"`,
+            `"nearest"`, `"wrap"` and `"reflect"`. Defaults to `"constant"`.
+            - `"reflect"`: `(d c b a | a b c d | d c b a)`
+                The input is extended by reflecting about the edge of the last
+                pixel.
+            - `"constant"`: `(k k k k | a b c d | k k k k)`
                 The input is extended by filling all values beyond
-                the edge with the same constant value k = 0.
-            - *wrap*: `(a b c d | a b c d | a b c d)` The input is extended by
-                wrapping around to the opposite edge.
-            - *nearest*: `(a a a a | a b c d | d d d d)`
+                the edge with the same constant value k specified by
+                `fill_value`.
+            - `"wrap"`: `(a b c d | a b c d | a b c d)`
+                The input is extended by wrapping around to the opposite edge.
+            - `"nearest"`: `(a a a a | a b c d | d d d d)`
                 The input is extended by the nearest pixel.
+            Note that when using torch backend, `"reflect"` is redirected to
+            `"mirror"` `(c d c b | a b c d | c b a b)` because torch does not
+            support `"reflect"`.
+            Note that torch backend does not support `"wrap"`.
         interpolation: Interpolation mode. Supported values: `"nearest"`,
             `"bilinear"`.
         seed: Integer. Used to create a random seed.
