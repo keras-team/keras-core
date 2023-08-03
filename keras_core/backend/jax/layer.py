70884379e3775cc7b1ec2f32c32c8f4d7b7e24be
Qianli Scott Zhu <scottzhu@google.com>
2023-08-03 23:57:13 +0000

    WIP

--- keras_core/backend/jax/layer.py
+++ keras_core/backend/jax/layer.py
@@ -1,4 +1,18 @@
+from keras_core.backend.common import distribute_scope
+
+
 class JaxLayer:
     def _post_build(self):
         """Can be overriden to perform post-build actions."""
-        pass
+        if not distribute_scope.in_distribute_scope():
+            return
+
+        distribution = distribute_scope.get_distribute_scope().distribute
+        # Swap both trainable/non-trainable weights,
+        for v in self._trainable_variables:
+            v.assign(distribution.distribute_weight(v))
+        for v in self._non_trainable_variables:
+            v.assign(distribution.distribute_weight(v))
+
+        # We don't care about the sub layers, which will be handled within the
+        # _post_build() by itself.
