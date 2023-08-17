c8df6b796a1a535278bcd5e78a6c509f78625996
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-08-17 23:06:53 +0000

    fix Black mistaks

--- keras_core/ops/numpy.py
+++ keras_core/ops/numpy.py
@@ -1326,17 +1326,16 @@ class Clip(Operation):
 def clip(x, x_min, x_max):
     """Clip (limit) the values in a tensor.
 
-        Given an interval, values outside the interval are clipped to the
-        interval edges. For example, if an interval of `[0, 1]` is specified,
-        values smaller than 0 become 0, and values larger than 1 become 1.
+    Given an interval, values outside the interval are clipped to the
+    interval edges. For example, if an interval of `[0, 1]` is specified,
+    values smaller than 0 become 0, and values larger than 1 become 1.
 
-        Args:
-            x: Input tensor.
-            x_min: Minimum value.
-            x_max: Maximum value.
-
-    pu    Returns:
-            The clipped tensor.
+    Args:
+        x: Input tensor.
+        x_min: Minimum value.
+        x_max: Maximum value.
+    Returns:
+        The clipped tensor.
     """
     if any_symbolic_tensors((x,)):
         return Clip(x_min, x_max).symbolic_call(x)
