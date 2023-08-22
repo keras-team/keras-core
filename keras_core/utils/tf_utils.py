5bfbe2526e48376c252d2dc0663986b8d2fcae8b
asingh9530 <abhinav199530singh@gmail.com>
2023-08-22 19:01:45 +0530

    removing tf.keras dependency

--- keras_core/utils/tf_utils.py
+++ keras_core/utils/tf_utils.py
@@ -141,7 +141,6 @@ def get_tensor_spec(t, dynamic_batch=False, name=None):
 def ensure_tensor(inputs, dtype=None):
     """Ensures the input is a Tensor, SparseTensor or RaggedTensor."""
     if not isinstance(inputs, (tf.Tensor, tf.SparseTensor, tf.RaggedTensor)):
-        print(dtype)
         inputs = tf.convert_to_tensor(inputs, dtype)
     if dtype is not None and inputs.dtype != dtype:
         inputs = tf.cast(inputs, dtype)
