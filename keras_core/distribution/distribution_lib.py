c231feb48ed7cd9d984269efdfac25744cdf04e1
Qianli Scott Zhu <scottzhu@google.com>
2023-08-22 02:39:29 +0000

    Addres review comments.

--- keras_core/distribution/distribution_lib.py
+++ keras_core/distribution/distribution_lib.py
@@ -7,22 +7,21 @@ will be implemented in future (via tf.dtensor API).
 """
 
 import contextlib
-from typing import Any
 
-from keras_core import KerasVariable
 from keras_core.backend.common import global_state
 
 GLOBAL_ATTRIBUTE_NAME = "distribution"
 
 
-def list_devices(device_type: str = None) -> list[str]:
+def list_devices(device_type=None):
     """Return all the available devices based on the device type.
 
     Note that this should return the global devices in a distributed setting.
 
     Args:
-        device_type: CPU, GPU or TPU. Default to GPU or TPU if available when
-            device_type is not provided. Otherwise will return the CPU devices.
+        device_type: string of 'CPU', 'GPU' or 'TPU'. Default to GPU or TPU if
+            available when device_type is not provided. Otherwise will return
+            the CPU devices.
 
     Return:
         List of devices that are available for distribute computation.
@@ -36,27 +35,29 @@ class DeviceMesh:
     This is aligned with `jax.sharding.Mesh` and `tf.dtensor.Mesh`, which
     represents the computation devices in the global context.
 
-    See more details in
-    https://jax.readthedocs.io/en/latest/jax.sharding.html#jax.sharding.Mesh
-    and https://www.tensorflow.org/api_docs/python/tf/experimental/dtensor/Mesh.
+    See more details in [jax.sharding.Mesh]
+    (https://jax.readthedocs.io/en/latest/jax.sharding.html#jax.sharding.Mesh)
+    and [tf.dtensor.Mesh]
+    (https://www.tensorflow.org/api_docs/python/tf/experimental/dtensor/Mesh).
     """
 
     def __init__(
         self,
-        shape: list | tuple,
-        axis_names: list[str],
-        devices: list[str] = None,
+        shape,
+        axis_names,
+        devices=None,
     ):
         """Initialize the DeviceMesh for the given topology.
 
         Args:
-            shape: the shape of the overall DeviceMesh, e.g. (8,) for a data
-                parallel only distribution, or (4, 2) for a model+data parallel
-                distribution.
-            axis_names: The logical name of the each axis for the DeviceMesh.
-                The length of the `axis_names` should match to the rank of the
-                `shape`. The `axis_names` will be used to match/create the
-                `TensorLayout` when distribute the data and weights.
+            shape: tuple of list of integers. The shape of the overall
+                DeviceMesh, e.g. `(8,)` for a data parallel only distribution,
+                or `(4, 2)` for a model+data parallel distribution.
+            axis_names: List of string. The logical name of the each axis for
+                the DeviceMesh. The length of the `axis_names` should match to
+                the rank of the `shape`. The `axis_names` will be used to
+                match/create the `TensorLayout` when distribute the data and
+                weights.
             devices: Optional list of devices. Default to all the available
                 devices locally from `list_devices()`.
         """
@@ -71,16 +72,17 @@ class TensorLayout:
     `DeviceMesh` and `TensorLayout`, the actual mapping between a Tensor to the
     physical devices can be determined.
 
-    See more details in
-    https://jax.readthedocs.io/en/latest/jax.sharding.html#jax.sharding.NamedSharding
-    and https://www.tensorflow.org/api_docs/python/tf/experimental/dtensor/Layout.  # noqa: E501
+    See more details in [jax.sharding.NamedSharding]
+    (https://jax.readthedocs.io/en/latest/jax.sharding.html#jax.sharding.NamedSharding)
+    and [tf.dtensor.Layout]
+    (https://www.tensorflow.org/api_docs/python/tf/experimental/dtensor/Layout).
     """
 
-    def __init__(self, layout_spec: list | tuple):
-        """Initialize the TensorLayout with layout_spec.
+    def __init__(self, axes):
+        """Initialize the TensorLayout with axis names.
 
         Args:
-            layout_spec: list of strings that should map to the `axis_names` in
+            axes: list of strings that should map to the `axis_names` in
                 `DeviceMesh`. For any dimentions that doesn't need any sharding,
                 A `None` can be used a placeholder.
         """
@@ -99,10 +101,10 @@ class Distribution:
     `Distribution` and distribute the variable/data accordingly.
     """
 
-    def __init__(self, device_mesh: DeviceMesh):
+    def __init__(self, device_mesh):
         pass
 
-    def distribute_data(self, data) -> Any:
+    def distribute_data(self, data):
         """Shard the input data based on the Distribution setting.
 
         Args:
@@ -113,7 +115,7 @@ class Distribution:
         """
         pass
 
-    def distribute_variable(self, variable: KerasVariable) -> KerasVariable:
+    def distribute_variable(self, variable):
         """Distribute the variable based on the Distribution setting.
 
         Args:
@@ -124,21 +126,17 @@ class Distribution:
         """
         pass
 
-    def as_global_distribution(self):
-        """Set the current `Distribution` as the global distribution setting."""
-        pass
-
     @contextlib.contextmanager
     def scope(self):
         """Context manager to make the `Distribution` current."""
         pass
 
 
-def get_global_distribution():
+def get_distribution():
     """Retrieve the current distribution from global context."""
     return global_state.get_global_attribute(GLOBAL_ATTRIBUTE_NAME)
 
 
-def set_global_distribution(distribution: Distribution):
+def set_distribution(distribution):
     """Set the distribution as the global distribution setting."""
-    distribution.as_global_distribution()
+    global_state.set_global_attribute(GLOBAL_ATTRIBUTE_NAME, distribution)
