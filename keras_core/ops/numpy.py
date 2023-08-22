506cbb7f00db618e4caee68b072e09b76321fceb
AakashKumarNain <aakashnain@outlook.com>
2023-08-22 23:34:57 +0530

    add checks for input arguments

--- keras_core/ops/numpy.py
+++ keras_core/ops/numpy.py
@@ -5034,6 +5034,11 @@ def vstack(xs):
 
 class Where(Operation):
     def call(self, condition, x1=None, x2=None):
+        if (x1 is None and x2 is not None) or (x1 is not None and x2 is None):
+            raise ValueError(
+                "`x1` and `x2` either both should be `None`"
+                + " or both should have non-None value."
+            )
         return backend.numpy.where(condition, x1, x2)
 
     def compute_output_spec(self, condition, x1, x2):
@@ -5059,6 +5064,11 @@ def where(condition, x1=None, x2=None):
         A tensor with elements from `x1` where `condition` is `True`, and
         elements from `x2` where `condition` is `False`.
     """
+    if (x1 is None and x2 is not None) or (x1 is not None and x2 is None):
+        raise ValueError(
+            "`x1` and `x2` either both should be `None`"
+            + " or both should have non-None value."
+        )
     if any_symbolic_tensors((condition, x1, x2)):
         return Where().symbolic_call(condition, x1, x2)
     return backend.numpy.where(condition, x1, x2)
