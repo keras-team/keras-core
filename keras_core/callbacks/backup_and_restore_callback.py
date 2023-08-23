70abdc7ed8c52172f1870f512fbff26d71c6bdc4
Adithya Kamath <kamathis4@gmail.com>
2023-08-23 19:09:58 +0530

    Comments fixed, tests written

--- keras_core/callbacks/backup_and_restore_callback.py
+++ keras_core/callbacks/backup_and_restore_callback.py
@@ -1,59 +1,83 @@
 import os
-
 import keras_core.saving
+
 from keras_core.api_export import keras_core_export
 from keras_core.callbacks.callback import Callback
 from keras_core.utils import file_utils
-from keras_core.utils import io_utils
 
 
 @keras_core_export("keras_core.callbacks.BackupAndRestoreCallback")
 class BackupAndRestoreCallback(Callback):
-    """
-    Callback to back up and restore the training state.
+    """Callback to back up and restore the training state.
 
-    BackupAndRestore callback is intended to recover training from an
-    interruption that has happened in the middle of a Model.fit execution,
-    by backing up the training states in a temporary checkpoint file at the
-    end of each epoch. Each backup overwrites the previously written
-    checkpoint file, so at any given time there is at most one such
-    checkpoint file for backup/restoring purpose.
+    `BackupAndRestore` callback is intended to recover training from an
+    interruption that has happened in the middle of a `Model.fit` execution, by
+    backing up the training states in a temporary checkpoint file (with the help
+    of a `tf.train.CheckpointManager`), at the end of each epoch. Each backup
+    overwrites the previously written checkpoint file, so at any given time
+    there is at most one such checkpoint file for backup/restoring purpose.
 
-    If training restarts before completion, the training state (which
-    includes the Model weights and epoch number) is restored to the most
-    recently saved state at the beginning of a new Model.fit run. At the
-    completion of a Model.fit run, the temporary checkpoint file is deleted.
+    If training restarts before completion, the training state (which includes
+    the `Model` weights and epoch number) is restored to the most recently saved
+    state at the beginning of a new `Model.fit` run. At the completion of a
+    `Model.fit` run, the temporary checkpoint file is deleted.
 
-    Note that the user is responsible to bring jobs back after the
-    interruption. This callback is important for the backup and restore
-    mechanism for fault tolerance purpose, and the model to be restored from
-    a previous checkpoint is expected to be the same as the one used to back
-    up. If user changes arguments passed to compile or fit, the checkpoint
-    saved for fault tolerance can become invalid.
+    Note that the user is responsible to bring jobs back after the interruption.
+    This callback is important for the backup and restore mechanism for fault
+    tolerance purpose, and the model to be restored from a previous checkpoint
+    is expected to be the same as the one used to back up. If user changes
+    arguments passed to compile or fit, the checkpoint saved for fault tolerance
+    can become invalid.
+
+    Example:
+
+    >>> class InterruptingCallback(tf.keras.callbacks.Callback):
+    ...   def on_epoch_begin(self, epoch, logs=None):
+    ...     if epoch == 4:
+    ...       raise RuntimeError('Interrupting!')
+    >>> callback = keras.callbacks.BackupAndRestore(backup_dir="/tmp/backup")
+    >>> model = keras.models.Sequential([tf.keras.layers.Dense(10)])
+    >>> model.compile(keras.optimizers.SGD(), loss='mse')
+    >>> try:
+    ...   model.fit(np.arange(100).reshape(5, 20), np.zeros(5), epochs=10,
+    ...             batch_size=1, callbacks=[callback, InterruptingCallback()],
+    ...             verbose=0)
+    ... except:
+    ...   pass
+    >>> history = model.fit(np.arange(100).reshape(5, 20), np.zeros(5),
+    ...                     epochs=10, batch_size=1, callbacks=[callback],
+    ...                     verbose=0)
+    >>> # Only 6 more epochs are run, since first training got interrupted at
+    >>> # zero-indexed epoch 4, second training will continue from 4 to 9.
+    >>> len(history.history['loss'])
+    6
 
     Args:
-        backup_dir: String, path to store the checkpoint. e.g. backup_dir =
-            os.path.join(working_dir, 'backup'). This is the directory in which
-            the system stores temporary files to recover the model from jobs
-            terminated unexpectedly.
-        save_freq: 'epoch', integer, or False. When set to 'epoch'
-            the callback saves the checkpoint at the end of each epoch. When set
-            to an integer, the callback saves the checkpoint every save_freq
-            batches. Set save_freq to False if only using preemption
-            checkpointing (with save_before_preemption=True).
-        delete_checkpoint: Boolean, default to True. This BackupAndRestore
-            callback works by saving a checkpoint to back up the training state.
-            If delete_checkpoint=True, the checkpoint will be deleted after
-            training is finished. Use False if you'd like to keep the checkpoint
-            for future usage.
+        file_path: String, path to store the checkpoint.
+          e.g. `backup_dir = os.path.join(working_dir, 'backup')`.
+          This is the directory in which the system stores temporary files to
+          recover the model from jobs terminated unexpectedly. The directory
+          cannot be reused elsewhere to store other files, e.g. by the
+          `BackupAndRestore` callback of another training run,
+          or by another callback
+          (e.g. `ModelCheckpoint`) of the same training.
+        save_freq: `'epoch'`, integer, or `False`. When set to `'epoch'`
+          the callback saves the checkpoint at the end of each epoch.
+          When set to an integer, the callback saves the checkpoint every
+          `save_freq` batches. Set `save_freq` to `False` if only using
+          preemption checkpointing (with `save_before_preemption=True`).
+        delete_checkpoint: Boolean, default to True. This `BackupAndRestore`
+          callback works by saving a checkpoint to back up the training state.
+          If `delete_checkpoint=True`, the checkpoint will be deleted after
+          training is finished. Use `False` if you'd like to keep the checkpoint
+          for future usage.
         save_before_preemption: A boolean value instructing whether to turn on
-            the automatic checkpoint saving for preemption/maintenance events.
-
+          the automatic checkpoint saving for preemption/maintenance events.
     """
 
     def __init__(
         self,
-        backup_dir,
+        file_path,
         save_freq="epoch",
         delete_checkpoint=True,
         save_before_preemption=False,
@@ -63,11 +87,12 @@ class BackupAndRestoreCallback(Callback):
         self.save_freq = save_freq
         self.delete_checkpoint = delete_checkpoint
         self.save_before_preemption = save_before_preemption
+        self._batches_seen_since_last_saving = 0
         self._last_batch_seen = 0
 
-        if not backup_dir:
+        if not file_path:
             raise ValueError("Empty `backup_dir` argument passed")
-        self.backup_dir = backup_dir
+        self.file_path = file_path
 
         if (not save_freq) and (not save_before_preemption):
             raise ValueError(
@@ -84,28 +109,25 @@ class BackupAndRestoreCallback(Callback):
         """
         Get training state from temporary file and restore it
         """
-        super().on_train_begin()
-        self.set_model(keras_core.saving.load_model(filepath=self.backup_dir))
+        if self._check_checkpoints_exists(self.file_path):
+            self._model = keras_core.saving.load_model(filepath=self.file_path)
 
     def on_train_end(self, logs=None):
-        """
-        Delete training state stored
-        """
-        if self._check_checkpoints_exists(self.backup_dir):
+        if self.delete_checkpoint and \
+                self._check_checkpoints_exists(self.file_path):
             self._cleanup_checkpoint()
 
     def on_epoch_begin(self, epoch, logs=None):
-        if self.delete_checkpoint:
+        if self.delete_checkpoint and \
+                self._check_checkpoints_exists(self.file_path):
             self._cleanup_checkpoint()
         self._current_epoch = epoch
 
     def on_epoch_end(self, epoch, logs=None):
-        super().on_epoch_end(epoch)
         if self.save_freq == "epoch":
             self._save_model(epoch=epoch, batch=None, logs=logs)
 
     def on_train_batch_end(self, batch, logs=None):
-        super().on_epoch_end(batch)
         if self._should_save_on_batch(batch):
             self._save_model(epoch=self._current_epoch, batch=batch, logs=logs)
 
@@ -128,10 +150,6 @@ class BackupAndRestoreCallback(Callback):
 
         try:
             self._model.save(filepath, overwrite=True)
-            if self.verbose > 0:
-                io_utils.print_msg(
-                    f"\nEpoch {epoch + 1}: saving model to {filepath}"
-                )
         except IsADirectoryError:  # h5py 3.x
             raise IOError(
                 "Please specify a non-directory filepath for "
@@ -159,14 +177,14 @@ class BackupAndRestoreCallback(Callback):
             # logged metrics and the path's placeholders can cause formatting to
             # fail.
             if batch is None or "batch" in logs:
-                file_path = self.filepath.format(epoch=epoch + 1, **logs)
+                file_path = self.file_path.format(epoch=epoch + 1, **logs)
             else:
-                file_path = self.filepath.format(
+                file_path = self.file_path.format(
                     epoch=epoch + 1, batch=batch + 1, **logs
                 )
         except KeyError as e:
             raise KeyError(
-                f'Failed to format this callback filepath: "{self.filepath}". '
+                f'Failed to format this callback filepath: "{self.file_path}". '
                 f"Reason: {e}"
             )
         return file_path
@@ -191,8 +209,8 @@ class BackupAndRestoreCallback(Callback):
         """
         Delete other checkpoint files (if present) in the directory
         """
-        if self._check_checkpoints_exists(filepath=self.backup_dir):
-            file_utils.rmtree(self.backup_dir)
+        if self._check_checkpoints_exists(filepath=self.file_path):
+            file_utils.rmtree(self.file_path)
 
     def _check_checkpoints_exists(self, filepath):
         return file_utils.exists(filepath)
--- keras_core/callbacks/backup_and_restore_callback_test.py
+++ keras_core/callbacks/backup_and_restore_callback_test.py
@@ -1,45 +1,55 @@
 import pytest
+import os
+import numpy as np
 
 from keras_core import callbacks
+from keras_core import layers
 from keras_core import testing
+from keras_core.models import Sequential
+from keras_core.utils import file_utils
+
+
+class InterruptingCallback(callbacks.Callback):
+    """A callback to intentionally introduce interruption to
+    training."""
+
+    def __init__(self, steps_int, epoch_int):
+        self.batch_count = 0
+        self.epoch_count = 0
+        self.steps_int = steps_int
+        self.epoch_int = epoch_int
+
+    def on_epoch_end(self, epoch, log=None):
+        self.epoch_count += 1
+        if self.epoch_int is not None and self.epoch_count == self.epoch_int:
+            raise RuntimeError("EpochInterruption")
+
+    def on_batch_end(self, batch, logs=None):
+        self.batch_count += 1
+        if self.steps_int is not None and self.batch_count == self.steps_int:
+            raise RuntimeError("StepsInterruption")
 
 
 class BackupAndRestoreCallbackTest(testing.TestCase):
-    class InterruptingCallback(callbacks.Callback):
-        """A callback to intentionally introduce interruption to
-        training."""
-
-        batch_count = 0
-        epoch_int = 2
-        steps_int = 5
-
-        def on_epoch_end(self, epoch, log=None):
-            if epoch == self.epoch_int:
-                raise RuntimeError("EpochInterruption")
-
-        def on_batch_end(self, batch, logs=None):
-            self.batch_count += 1
-            if self.batch_count == self.steps_int:
-                raise RuntimeError("StepsInterruption")
 
     # Checking for invalid backup_dir
     def test_empty_backup_dir(self):
         with self.assertRaisesRegex(
-            ValueError, expected_regex="Empty " "`backup_dir`"
+                ValueError, expected_regex="Empty " "`backup_dir`"
         ):
-            callbacks.BackupAndRestoreCallback(backup_dir=None)
+            callbacks.BackupAndRestoreCallback(file_path=None)
 
     # Checking save_freq and save_before_preemption both unset
     def test_save_set_error(self):
         with self.assertRaisesRegex(
-            ValueError,
-            expected_regex="`save_freq` or "
-            "`save_before_preemption` "
-            ""
-            "must be set",
+                ValueError,
+                expected_regex="`save_freq` or "
+                               "`save_before_preemption` "
+                               ""
+                               "must be set",
         ):
             callbacks.BackupAndRestoreCallback(
-                backup_dir="backup_dir",
+                file_path="backup_dir",
                 save_freq=None,
                 save_before_preemption=False,
             )
@@ -47,21 +57,184 @@ class BackupAndRestoreCallbackTest(testing.TestCase):
     # Check invalid save_freq, both string and non integer
     def test_save_freq_unknown_error(self):
         with self.assertRaisesRegex(
-            ValueError, expected_regex="Unrecognized save_freq"
+                ValueError, expected_regex="Unrecognized save_freq"
         ):
             callbacks.BackupAndRestoreCallback(
-                backup_dir="backup_dir", save_freq="batch"
+                file_path="backup_dir", save_freq="batch"
             )
 
         with self.assertRaisesRegex(
-            ValueError, expected_regex="Unrecognized save_freq"
+                ValueError, expected_regex="Unrecognized save_freq"
         ):
             callbacks.BackupAndRestoreCallback(
-                backup_dir="backup_dir", save_freq=0.15
+                file_path="backup_dir", save_freq=0.15
             )
 
-    # Checking if after interruption, correct model params and weights are
-    # loaded
+    # Checking if after interruption, correct model params and
+    # weights are loaded in step-wise backup
     @pytest.mark.requires_trainable_backend
-    def test_best_case(self):
-        pass
+    def test_best_case_step(self):
+        def make_model():
+            np.random.seed(1337)
+            model = Sequential(
+                [
+                    layers.Dense(2, activation="relu"),
+                    layers.Dense(1),
+                ]
+            )
+            model.compile(
+                loss="mse",
+                optimizer="sgd",
+                metrics=["mse"],
+            )
+            return model
+
+        temp_dir = self.get_temp_dir()
+        filepath = os.path.join(temp_dir, "subdir", "checkpoint.keras")
+        file_utils.rmtree(filepath)
+        self.assertFalse(os.path.exists(filepath))
+
+        model = make_model()
+        cbk = callbacks.BackupAndRestoreCallback(
+            file_path=filepath, save_freq=1
+        )
+
+        x_train = np.random.random((10, 3))
+        y_train = np.random.random((10, 1))
+
+        try:
+            model.fit(
+                x_train,
+                y_train,
+                batch_size=4,
+                callbacks=[cbk,
+                           InterruptingCallback(steps_int=2, epoch_int=None)],
+                epochs=2,
+                verbose=0,
+            )
+        except RuntimeError:
+            self.assertTrue(os.path.exists(filepath))
+            self.assertEqual(cbk._current_epoch, 0)
+            self.assertEqual(cbk._last_batch_seen, 1)
+
+            hist = model.fit(
+                x_train,
+                y_train,
+                batch_size=4,
+                callbacks=[cbk],
+                epochs=5
+            )
+
+            self.assertEqual(cbk._current_epoch, 4)
+            self.assertEqual(hist.epoch[-1], 4)
+
+    # Checking if after interruption, correct model params and
+    # weights are loaded in epoch-wise backup
+    @pytest.mark.requires_trainable_backend
+    def test_best_case_epoch(self):
+        def make_model():
+            np.random.seed(1337)
+            model = Sequential(
+                [
+                    layers.Dense(2, activation="relu"),
+                    layers.Dense(1),
+                ]
+            )
+            model.compile(
+                loss="mse",
+                optimizer="sgd",
+                metrics=["mse"],
+            )
+            return model
+
+        temp_dir = self.get_temp_dir()
+        filepath = os.path.join(temp_dir, "subdir", "checkpoint.keras")
+        file_utils.rmtree(filepath)
+        self.assertFalse(os.path.exists(filepath))
+
+        model = make_model()
+        cbk = callbacks.BackupAndRestoreCallback(
+            file_path=filepath, save_freq="epoch"
+        )
+
+        x_train = np.random.random((10, 3))
+        y_train = np.random.random((10, 1))
+
+        try:
+            model.fit(
+                x_train,
+                y_train,
+                batch_size=4,
+                callbacks=[cbk,
+                           InterruptingCallback(steps_int=None, epoch_int=2)],
+                epochs=6,
+                verbose=0,
+            )
+        except RuntimeError:
+            self.assertEqual(cbk._current_epoch, 1)
+            self.assertTrue(os.path.exists(filepath))
+
+            hist = model.fit(
+                x_train,
+                y_train,
+                batch_size=4,
+                callbacks=[cbk],
+                epochs=5
+            )
+            self.assertEqual(cbk._current_epoch, 4)
+            self.assertEqual(hist.epoch[-1], 4)
+
+    # Checking if after interruption, when model is deleted
+    @pytest.mark.requires_trainable_backend
+    def test_model_deleted_case_epoch(self):
+        def make_model():
+            np.random.seed(1337)
+            model = Sequential(
+                [
+                    layers.Dense(2, activation="relu"),
+                    layers.Dense(1),
+                ]
+            )
+            model.compile(
+                loss="mse",
+                optimizer="sgd",
+                metrics=["mse"],
+            )
+            return model
+
+        temp_dir = self.get_temp_dir()
+        filepath = os.path.join(temp_dir, "subdir", "checkpoint.keras")
+        file_utils.rmtree(filepath)
+        self.assertFalse(os.path.exists(filepath))
+
+        model = make_model()
+        cbk = callbacks.BackupAndRestoreCallback(
+            file_path=filepath, save_freq="epoch"
+        )
+
+        x_train = np.random.random((10, 3))
+        y_train = np.random.random((10, 1))
+
+        try:
+            model.fit(
+                x_train,
+                y_train,
+                batch_size=4,
+                callbacks=[cbk,
+                           InterruptingCallback(steps_int=None, epoch_int=2)],
+                epochs=6,
+                verbose=0,
+            )
+        except RuntimeError:
+            self.assertEqual(cbk._current_epoch, 1)
+            self.assertTrue(os.path.exists(filepath))
+            file_utils.rmtree(filepath)
+
+            model.fit(
+                x_train,
+                y_train,
+                batch_size=4,
+                callbacks=[cbk],
+                epochs=5
+            )
+            self.assertEqual(cbk._current_epoch, 4)
