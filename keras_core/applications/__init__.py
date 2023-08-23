4aefffff58f3ef392f9517b7e00a4f5e0ab8c471
Akorede Adewole <akoredeadewole8@gmail.com>
2023-08-23 12:11:03 +0100

    added imports for keras applications models

---
+++ keras_core/applications/__init__.py
@@ -0,0 +1,19 @@
+from keras_core.applications.convnext import ConvNeXtTiny
+from keras_core.applications.convnext import ConvNeXtSmall
+from keras_core.applications.convnext import ConvNeXtBase
+from keras_core.applications.convnext import ConvNeXtLarge
+from keras_core.applications.convnext import ConvNeXtXLarge
+
+from keras_core.applications.densenet import DenseNet121
+from keras_core.applications.densenet import DenseNet169
+from keras_core.applications.densenet import DenseNet201
+
+from keras_core.applications.efficientnet import EfficientNetB0
+
+from keras_core.applications.vgg16 import VGG16
+from keras_core.applications.vgg16 import preprocess_input
+from keras_core.applications.vgg16 import decode_predictions
+
+from keras_core.applications.vgg19 import VGG19
+from keras_core.applications.vgg19 import preprocess_input
+from keras_core.applications.vgg19 import decode_predictions
