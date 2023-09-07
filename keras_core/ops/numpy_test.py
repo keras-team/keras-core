dae90204ba87d37859c18a2b96baacd0a18f5a90
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-07 14:25:23 +0000

    Add-more-tests-in-ops/numpy_test.py

--- keras_core/ops/numpy_test.py
+++ keras_core/ops/numpy_test.py
@@ -295,16 +295,15 @@ class NumpyTwoInputOpsDynamicShapeTest(testing.TestCase):
         y = KerasTensor([3, 6, 5]).shape
         self.assertTrue(knp.shape_equal(x, y, axis=[1]))
 
-    # TODO Why only jax failed this test ?
     def test_shape_non_equal_with_negative_axis(self):
         x = KerasTensor([3, 4, 5]).shape
         y = KerasTensor([3, 4, 6]).shape
         self.assertFalse(knp.shape_equal(x, y, axis=-2))
 
-    # def test_shape_equal_with_negative_axis(self):
-    #     x = KerasTensor([3, 4, 5]).shape
-    #     y = KerasTensor([3, 4, 5]).shape
-    #     self.assertTrue(knp.shape_equal(x, y, axis=-1))
+    def test_shape_equal_with_negative_axis(self):
+        x = KerasTensor([3, 4, 5]).shape
+        y = KerasTensor([3, 4, 5]).shape
+        self.assertTrue(knp.shape_equal(x, y, axis=-1))
 
     def test_shape_equal_zeros(self):
         x = KerasTensor([0, 4]).shape
