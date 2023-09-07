d32239a91337f990c252cd4c08d6011cffad7358
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-07 20:13:05 +0000

    Increase test coverage

--- keras_core/activations/activations.py
+++ keras_core/activations/activations.py
@@ -74,31 +74,36 @@ class ReLU(ops.Operation):
 
     @staticmethod
     def static_call(x, negative_slope=0.0, max_value=None, threshold=0.0):
-        # Handle negative slope
         if negative_slope != 0.0:
+            if max_value is None and threshold == 0:
+                return backend.nn.leaky_relu(x, negative_slope=negative_slope)
+
             if threshold != 0:
                 negative_part = backend.nn.relu(-x + threshold)
             else:
                 negative_part = backend.nn.relu(-x)
 
-        # Handle threshold
+        clip_max = max_value is not None
         if threshold != 0:
-            x = backend.numpy.where(x >= threshold, x, 0.0)
-
-        # Handle max value (Use relu6 for max_value=6)
-        if max_value == 6:
+            # computes x for x > threshold else 0
+            threshold = ops.cast(threshold, dtype=x.dtype)
+            x = x * backend.cast(
+                backend.numpy.greater(x, threshold), dtype=x.dtype
+            )
+        elif max_value == 6:
+            # if no threshold, then can use nn.relu6 native op for performance
             x = backend.nn.relu6(x)
-        elif max_value is not None:
-            x = backend.numpy.clip(x, 0, max_value)
+            clip_max = False
         else:
             x = backend.nn.relu(x)
 
-        # Apply negative slope to the negative part
-        if negative_slope != 0.0:
-            x = backend.numpy.where(
-                x > 0, x, x - negative_slope * negative_part
-            )
+        if clip_max:
+            min_value = ops.cast(0.0, dtype=x.dtype)
+            max_value = ops.cast(max_value, dtype=x.dtype)
+            x = backend.numpy.clip(x, min_value, max_value)
 
+        if negative_slope != 0.0:
+            x -= negative_slope * negative_part
         return x
 
 
