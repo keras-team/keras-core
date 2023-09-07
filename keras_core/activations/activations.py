2f0e547827bcd3734133f1bbed55a6224c0b813a
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-07 15:53:30 +0000

    Fix ReLU Negative Values with max_value Specified

--- keras_core/activations/activations.py
+++ keras_core/activations/activations.py
@@ -74,37 +74,61 @@ class ReLU(ops.Operation):
 
     @staticmethod
     def static_call(x, negative_slope=0.0, max_value=None, threshold=0.0):
+        # Handle negative_slope first
+        negative_part = None
         if negative_slope != 0.0:
-            if max_value is None and threshold == 0:
-                return backend.nn.leaky_relu(x, negative_slope=negative_slope)
-
             if threshold != 0:
                 negative_part = backend.nn.relu(-x + threshold)
             else:
                 negative_part = backend.nn.relu(-x)
 
-        clip_max = max_value is not None
-        if threshold != 0:
-            # computes x for x > threshold else 0
-            threshold = ops.cast(threshold, dtype=x.dtype)
-            x = x * backend.cast(
-                backend.numpy.greater(x, threshold), dtype=x.dtype
-            )
-        elif max_value == 6:
-            # if no threshold, then can use nn.relu6 native op for performance
-            x = backend.nn.relu6(x)
-            clip_max = False
-        else:
-            x = backend.nn.relu(x)
+        # Now, apply ReLU activation to the positive part only
+        positive_part = backend.nn.relu(x)
 
-        if clip_max:
-            min_value = ops.cast(0.0, dtype=x.dtype)
-            max_value = ops.cast(max_value, dtype=x.dtype)
-            x = backend.numpy.clip(x, min_value, max_value)
+        if max_value is not None:
+            positive_part = backend.numpy.clip(positive_part, 0, max_value)
 
+        # Combine the positive and negative parts
         if negative_slope != 0.0:
-            x -= negative_slope * negative_part
+            x = positive_part - negative_slope * negative_part
+        else:
+            x = positive_part
+
         return x
+
+    # @staticmethod
+    # def static_call(x, negative_slope=0.0, max_value=None, threshold=0.0):
+    #     if negative_slope != 0.0:
+    #         if max_value is None and threshold == 0:
+    #             return backend.nn.leaky_relu(x, negative_slope=negative_slope)
+
+    #         if threshold != 0:
+    #             negative_part = backend.nn.relu(-x + threshold)
+    #         else:
+    #             negative_part = backend.nn.relu(-x)
+
+    #     clip_max = max_value is not None
+    #     if threshold != 0:
+    #         # computes x for x > threshold else 0
+    #         threshold = ops.cast(threshold, dtype=x.dtype)
+    #         x = x * backend.cast(
+    #             backend.numpy.greater(x, threshold), dtype=x.dtype
+    #         )
+    #     elif max_value == 6:
+    #         # if no threshold, then can use nn.relu6 native op for performance
+    #         x = backend.nn.relu6(x)
+    #         clip_max = False
+    #     else:
+    #         x = backend.nn.relu(x)
+
+    #     if clip_max:
+    #         min_value = ops.cast(0.0, dtype=x.dtype)
+    #         max_value = ops.cast(max_value, dtype=x.dtype)
+    #         x = backend.numpy.clip(x, min_value, max_value)
+
+    #     if negative_slope != 0.0:
+    #         x -= negative_slope * negative_part
+    #     return x
 
 
 @keras_core_export("keras_core.activations.leaky_relu")
