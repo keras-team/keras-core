4e017e5f5c5e4fff1ed6271dca705118186b58f2
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-08 21:18:03 +0000

    increase test coverage

--- keras_core/utils/python_utils_test.py
+++ keras_core/utils/python_utils_test.py
@@ -20,3 +20,24 @@ class PythonUtilsTest(testing.TestCase):
         x = "model.keras"
         self.assertEqual(python_utils.removeprefix(x, "model"), ".keras")
         self.assertEqual(python_utils.removeprefix(x, ".keras"), x)
+
+    def test_remove_long_seq(self):
+        sequences = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+        labels = [1, 2, 3]
+        new_sequences, new_labels = python_utils.remove_long_seq(
+            3, sequences, labels
+        )
+        self.assertEqual(new_sequences, [[1, 2, 3], [4, 5]])
+        self.assertEqual(new_labels, [1, 2])
+
+    def test_func_load_defaults_as_tuple(self):
+        # Using tuple as a default argument
+        def dummy_function(x=(1, 2, 3)):
+            pass
+
+        serialized = python_utils.func_dump(dummy_function)
+        deserialized = python_utils.func_load(serialized)
+        # Ensure that the defaults are still a tuple
+        self.assertIsInstance(deserialized.__defaults__[0], tuple)
+        # Ensure that the tuple default remains unchanged
+        self.assertEqual(deserialized.__defaults__[0], (1, 2, 3))
