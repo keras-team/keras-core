9fb59b6d9871990e629e00f4ca436896f435dbad
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-09 05:33:16 +0000

    Increase-test-coverage

--- keras_core/utils/python_utils.py
+++ keras_core/utils/python_utils.py
@@ -118,7 +118,7 @@ def remove_long_seq(maxlen, seq, label):
     """
     new_seq, new_label = [], []
     for x, y in zip(seq, label):
-        if len(x) <= maxlen:
+        if len(x) < maxlen:
             new_seq.append(x)
             new_label.append(y)
     return new_seq, new_label
