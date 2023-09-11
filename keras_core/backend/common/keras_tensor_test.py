eb8393bd67bba913599f9dbf2cb27d0e62ecd513
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-11 16:06:31 +0000

    Increase-test-coverage-in-`keras_core/backend

--- keras_core/backend/common/keras_tensor_test.py
+++ keras_core/backend/common/keras_tensor_test.py
@@ -89,3 +89,72 @@ class KerasTensorTest(testing.TestCase):
         abs_x = abs(x)  # this will internally call x.__abs__()
         mock_symbolic_call.assert_called_once_with(x)
         self.assertEqual(abs_x, mock_tensor)
+
+    @patch("keras_core.ops.Absolute.symbolic_call")
+    def test_abs_method(self, mock_method):
+        self._test_unary_op_method(mock_method, abs)
+
+    @patch("keras_core.ops.Negative.symbolic_call")
+    def test_neg_method(self, mock_method):
+        self._test_unary_op_method(mock_method, lambda x: -x)
+
+    @patch("keras_core.ops.Subtract.symbolic_call")
+    def test_sub_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x - y)
+
+    @patch("keras_core.ops.Multiply.symbolic_call")
+    def test_mul_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x * y)
+
+    @patch("keras_core.ops.Matmul.symbolic_call")
+    def test_matmul_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x @ y)
+
+    @patch("keras_core.ops.Power.symbolic_call")
+    def test_pow_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x**y)
+
+    @patch("keras_core.ops.Mod.symbolic_call")
+    def test_mod_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x % y)
+
+    @patch("keras_core.ops.Less.symbolic_call")
+    def test_lt_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x < y)
+
+    @patch("keras_core.ops.LogicalAnd.symbolic_call")
+    def test_and_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x & y)
+
+    @patch("keras_core.ops.LogicalOr.symbolic_call")
+    def test_or_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x | y)
+
+    @patch("keras_core.ops.GetItem.symbolic_call")
+    def test_getitem_method(self, mock_method):
+        y = Mock()
+        self._test_binary_op_method(mock_method, y, lambda x, y: x[y])
+
+    def _test_unary_op_method(self, mock_method, operator):
+        mock_tensor = Mock()
+        mock_method.return_value = mock_tensor
+        x = keras_tensor.KerasTensor(shape=(3, 4), dtype="float32")
+        result = operator(x)
+        mock_method.assert_called_once_with(x)
+        self.assertEqual(result, mock_tensor)
+
+    def _test_binary_op_method(self, mock_method, other, operator):
+        mock_tensor = Mock()
+        mock_method.return_value = mock_tensor
+        x = keras_tensor.KerasTensor(shape=(3, 4), dtype="float32")
+        result = operator(x, other)
+        mock_method.assert_called_once_with(x, other)
+        self.assertEqual(result, mock_tensor)
