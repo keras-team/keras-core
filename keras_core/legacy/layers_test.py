d0f7dac933420dd0296145d6e7a852d1ab10474c
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-11 06:29:32 +0000

    Increase-test-coverage-in-keras_core/legacy

--- keras_core/legacy/layers_test.py
+++ keras_core/legacy/layers_test.py
@@ -1,5 +1,6 @@
-import numpy as np
 import unittest
+
+import numpy as np
 
 from keras_core.legacy.layers import (
     AlphaDropout,
@@ -10,37 +11,18 @@ from keras_core.legacy.layers import (
 from keras_core.legacy.losses import Reduction
 
 
-class TestReduction(unittest.TestCase):
-    def test_all(self):
-        self.assertEqual(
-            Reduction.all(), ("auto", "none", "sum", "sum_over_batch_size")
-        )
-
-    def test_validate_valid_key(self):
-        Reduction.validate("auto")
-        Reduction.validate("none")
-        Reduction.validate("sum")
-        Reduction.validate("sum_over_batch_size")
-
-    def test_validate_invalid_key(self):
-        with self.assertRaisesRegex(
-            ValueError, "Invalid Reduction Key: invalid_key. Expected keys are"
-        ):
-            Reduction.validate("invalid_key")
-
-
 class TestAlphaDropout(unittest.TestCase):
     def test_call(self):
-        alpha_dropout = AlphaDropout(rate=0.5)
+        alpha_dropout = AlphaDropout(rate=0.5, seed=0)
         inputs = np.ones((2, 3, 4))
         output = alpha_dropout(inputs, training=True)
         self.assertEqual(output.shape, inputs.shape)
 
     def test_get_config(self):
-        alpha_dropout = AlphaDropout(rate=0.5)
+        alpha_dropout = AlphaDropout(rate=0.5, seed=0)
         config = alpha_dropout.get_config()
         self.assertEqual(config["rate"], 0.5)
-        self.assertIsNone(config["seed"])
+        self.assertEqual(config["seed"], 0)
 
 
 class TestRandomHeight(unittest.TestCase):
@@ -48,7 +30,7 @@ class TestRandomHeight(unittest.TestCase):
         random_height = RandomHeight(factor=0.5)
         inputs = np.ones((2, 3, 4))
         output = random_height(inputs, training=True)
-        self.assertEqual(output.shape, inputs.shape)
+        self.assertEqual(output.shape, (2, 3, 4))
 
     def test_get_config(self):
         random_height = RandomHeight(factor=0.5)
@@ -63,7 +45,7 @@ class TestRandomWidth(unittest.TestCase):
         random_width = RandomWidth(factor=0.5)
         inputs = np.ones((2, 3, 4))
         output = random_width(inputs, training=True)
-        self.assertEqual(output.shape, inputs.shape)
+        self.assertEqual(output.shape, (2, 3, 4))
 
     def test_get_config(self):
         random_width = RandomWidth(factor=0.5)
@@ -78,7 +60,7 @@ class TestThresholdedReLU(unittest.TestCase):
         thresholded_relu = ThresholdedReLU(theta=0.5)
         inputs = np.ones((2, 3, 4))
         output = thresholded_relu(inputs)
-        self.assertEqual(output.shape, inputs.shape)
+        self.assertEqual(output.shape, (2, 3, 4))
 
     def test_get_config(self):
         thresholded_relu = ThresholdedReLU(theta=0.5)
