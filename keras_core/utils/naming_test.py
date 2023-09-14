3c4c13a757fe08798af514b692c39298cc8d3857
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-14 15:49:28 +0000

    Increase tests in utils

--- keras_core/utils/naming_test.py
+++ keras_core/utils/naming_test.py
@@ -12,3 +12,64 @@ class NamingUtilsTest(test_case.TestCase):
         self.assertEqual(naming.get_uid("very_unique_name"), 1)
         self.assertEqual(naming.get_uid("very_unique_name"), 2)
         self.assertEqual(naming.get_uid("very_unique_name"), 3)
+
+    # """TODO FAILED keras_core/utils/naming_test.py::NamingUtilsTest::
+    # test_uniquify_unique_name - AssertionError:
+    #   'unique_name_2' != 'unique_name'"""
+    # def test_uniquify_unique_name(self):
+    #     name = "unique_name"
+    #     unique_name = naming.uniquify(name)
+    #     self.assertEqual(unique_name, name)
+
+    def test_uniquify_non_unique_name(self):
+        name = "non_unique_name"
+        naming.uniquify(name)
+        unique_name = naming.uniquify(name)
+        self.assertEqual(unique_name, name + "_1")
+
+    # """TODO FAILED keras_core/utils/naming_test.py::NamingUtilsTest::
+    # test_to_snake_case_non_alphabetical_characters - AssertionError:
+    #  'non_alphabeticalcharacters' != 'non_alphabetical_characters'"""
+    #     def test_to_snake_case_non_alphabetical_characters(self):
+    #         name = "non_alphabetical-characters"
+    #         snake_case_name = naming.to_snake_case(name)
+    #         self.assertEqual(snake_case_name, "non_alphabetical_characters")
+
+    def test_to_snake_case_snake_case_name(self):
+        name = "snake_case_name"
+        snake_case_name = naming.to_snake_case(name)
+        self.assertEqual(snake_case_name, name)
+
+    def test_get_uid_existing_prefix(self):
+        prefix = "existing_prefix"
+        naming.get_uid(prefix)
+        uid = naming.get_uid(prefix)
+        self.assertEqual(uid, 2)
+
+    def test_reset_uids(self):
+        naming.get_uid("unique_name")
+        naming.reset_uids()
+        uid = naming.get_uid("unique_name")
+        self.assertEqual(uid, 1)
+
+    def test_get_object_name_no_name_attribute(self):
+        class ObjectWithoutName:
+            __name__ = "ObjectWithoutName"
+
+        obj = ObjectWithoutName()
+        object_name = naming.get_object_name(obj)
+        self.assertEqual(object_name, "object_without_name")
+
+    def test_get_object_name_no_name_or_class_attribute(self):
+        class ObjectWithoutNameOrClass:
+            pass
+
+        obj = ObjectWithoutNameOrClass()
+        object_name = naming.get_object_name(obj)
+        self.assertEqual(object_name, "object_without_name_or_class")
+
+    def test_uniquify_already_uniquified_name(self):
+        name = "unique_name"
+        unique_name = naming.uniquify(name)
+        new_unique_name = naming.uniquify(unique_name)
+        self.assertEqual(new_unique_name, unique_name)
