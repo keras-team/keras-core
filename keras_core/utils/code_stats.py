e01699997ece90471f3ac4996dc54fe7d25e3fe8
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-14 06:21:39 +0000

    Increase tests in utils + Fix Bug in `count_loc`

--- keras_core/utils/code_stats.py
+++ keras_core/utils/code_stats.py
@@ -1,7 +1,28 @@
 import os
 
 
-def count_loc(directory, exclude=("_test",), extensions=(".py",), verbose=0):
+def count_loc(
+    directory: str,
+    exclude: tuple = ("_test",),
+    extensions: tuple = (".py",),
+    verbose: int = 0,
+) -> int:
+    """Count the Lines of Code (LOC) in a directory.
+
+    This function traverses a specified directory and calculates the number of lines of code
+    present in files with given extensions. It excludes lines from multiline strings,
+    comments, and blank lines. The function also provides the ability to exclude certain
+    files or directories.
+
+    Args:
+        directory (str): Path of the directory to count LOC.
+        exclude (tuple): Patterns to exclude files or directories. Default is `("_test",)`.
+        extensions (tuple): File extensions to consider for counting. Default is `(".py",)`.
+        verbose (int): If set to 1, displays the files being processed. Defaults to 0.
+
+    Returns:
+        int: Total number of lines of code in the directory.
+    """
     loc = 0
     for root, _, fnames in os.walk(directory):
         skip = False
@@ -17,7 +38,6 @@ def count_loc(directory, exclude=("_test",), extensions=(".py",), verbose=0):
                 if not fname.endswith(ext):
                     skip = True
                     break
-
                 for ex in exclude:
                     if fname.endswith(ex + ext):
                         skip = True
@@ -37,13 +57,24 @@ def count_loc(directory, exclude=("_test",), extensions=(".py",), verbose=0):
                 line = line.strip()
                 if not line or line.startswith("#"):
                     continue
-                if not string_open:
-                    if not line.startswith('"""'):
-                        loc += 1
-                    else:
-                        if not line.endswith('"""'):
-                            string_open = True
-                else:
-                    if line.startswith('"""'):
+
+                # Start of a multiline string
+                if line.startswith('"""') and not string_open:
+                    string_open = True
+
+                    # If the line ends the multiline string as well
+                    if line.count('"""') == 2:
                         string_open = False
+                        continue
+                    else:
+                        continue
+
+                # End of a multiline string
+                if line.endswith('"""') and string_open:
+                    string_open = False
+                    continue
+
+                # Counting LOC when not inside a multiline string
+                if not string_open:
+                    loc += 1
     return loc
---
+++ keras_core/utils/code_stats_test.py
@@ -0,0 +1,103 @@
+import os
+
+from keras_core.testing import test_case
+from keras_core.utils.code_stats import count_loc
+
+
+class TestCountLoc(test_case.TestCase):
+    def setUp(self):
+        self.test_dir = "test_directory"
+        os.makedirs(self.test_dir, exist_ok=True)
+
+    def tearDown(self):
+        for root, dirs, files in os.walk(self.test_dir, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+
+    def create_file(self, filename, content):
+        with open(
+            os.path.join(self.test_dir, filename), "w", encoding="utf-8"
+        ) as f:
+            f.write(content)
+
+    def test_count_loc_valid_python(self):
+        self.create_file(
+            "sample.py", "# This is a test file\n\nprint('Hello')\n"
+        )
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 1)
+
+    def test_exclude_test_files(self):
+        self.create_file("sample_test.py", "print('Hello')\n")
+        loc = count_loc(self.test_dir, exclude=("_test",))
+        self.assertEqual(loc, 0)
+
+    def test_other_extensions(self):
+        self.create_file("sample.txt", "Hello\n")
+        loc = count_loc(self.test_dir, extensions=(".py",))
+        self.assertEqual(loc, 0)
+
+    def test_comment_lines(self):
+        self.create_file(
+            "sample.py", "# Comment\nprint('Hello')\n# Another comment\n"
+        )
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 1)
+
+    def test_multiline_strings(self):
+        content = '"""\nMultiline string\nAnother line\n"""\nprint("Outside string")\n'
+        self.create_file("sample.py", content)
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 1)
+
+    def test_partial_multiline_strings(self):
+        content = '"""\nMultiline string not ending in this file.'
+        self.create_file("sample.py", content)
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 0)
+
+    def test_empty_file(self):
+        self.create_file("empty.py", "")
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 0)
+
+    def test_whitespace_only(self):
+        self.create_file("whitespace.py", "     \n\t\n")
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 0)
+
+    def test_nested_multiline_strings(self):
+        content = '''print("Start")
+        """
+        First level multiline
+        """
+        Second line outside any multiline
+        """
+        First level multiline
+        """Some inline code here"""
+        Nested multiline
+        """
+        print("End")
+        '''
+        self.create_file("nested_sample.py", content)
+        loc = count_loc(self.test_dir)
+        self.assertEqual(
+            loc, 3
+        )  # Only the print("Start"), Second line outside any multiline, and print("End")
+
+    def test_inline_comments_after_code(self):
+        content = 'print("Hello") # This is an inline comment'
+        self.create_file("inline_comment_sample.py", content)
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 1)  # The comment shouldn't affect the count
+
+    def test_directory_structure(self):
+        content1 = 'print("Hello from file1")'
+        content2 = 'print("Hello from file2")'
+        os.mkdir(os.path.join(self.test_dir, "subdir"))
+        self.create_file("sample1.py", content1)
+        self.create_file(os.path.join("subdir", "sample2.py"), content2)
+        loc = count_loc(self.test_dir)
+        self.assertEqual(loc, 2)  # Both files should be counted
