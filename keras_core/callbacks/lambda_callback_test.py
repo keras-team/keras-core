656d80dd1c7138e439b817b617d20aee46b661c3
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-16 12:00:50 +0000

    Add tests for `LambdaCallback`

--- keras_core/callbacks/lambda_callback_test.py
+++ keras_core/callbacks/lambda_callback_test.py
@@ -44,3 +44,75 @@ class LambdaCallbackTest(testing.TestCase):
             self.assertTrue(any("on_epoch_begin" in log for log in logs.output))
             self.assertTrue(any("on_epoch_end" in log for log in logs.output))
             self.assertTrue(any("on_train_end" in log for log in logs.output))
+
+    @pytest.mark.requires_trainable_backend
+    def test_LambdaCallback_with_batches(self):
+        BATCH_SIZE = 4
+        model = Sequential(
+            [layers.Input(shape=(2,), batch_size=BATCH_SIZE), layers.Dense(1)]
+        )
+        model.compile(
+            optimizer=optimizers.SGD(), loss=losses.MeanSquaredError()
+        )
+        x = np.random.randn(16, 2)
+        y = np.random.randn(16, 1)
+        lambda_log_callback = callbacks.LambdaCallback(
+            on_train_batch_begin=lambda batch, logs: logging.warning(
+                "on_train_batch_begin"
+            ),
+            on_train_batch_end=lambda batch, logs: logging.warning(
+                "on_train_batch_end"
+            ),
+        )
+        with self.assertLogs(level="WARNING") as logs:
+            model.fit(
+                x,
+                y,
+                batch_size=BATCH_SIZE,
+                validation_split=0.2,
+                callbacks=[lambda_log_callback],
+                epochs=5,
+                verbose=0,
+            )
+            self.assertTrue(
+                any("on_train_batch_begin" in log for log in logs.output)
+            )
+            self.assertTrue(
+                any("on_train_batch_end" in log for log in logs.output)
+            )
+
+    @pytest.mark.requires_trainable_backend
+    def test_LambdaCallback_with_kwargs(self):
+        BATCH_SIZE = 4
+        model = Sequential(
+            [layers.Input(shape=(2,), batch_size=BATCH_SIZE), layers.Dense(1)]
+        )
+        model.compile(
+            optimizer=optimizers.SGD(), loss=losses.MeanSquaredError()
+        )
+        x = np.random.randn(16, 2)
+        y = np.random.randn(16, 1)
+        model.fit(
+            x, y, batch_size=BATCH_SIZE, epochs=1, verbose=0
+        )  # Train briefly for evaluation to work.
+
+        custom_on_test_begin = lambda logs: logging.warning(
+            "custom_on_test_begin_executed"
+        )
+        lambda_log_callback = callbacks.LambdaCallback(
+            on_test_begin=custom_on_test_begin
+        )
+        with self.assertLogs(level="WARNING") as logs:
+            model.evaluate(
+                x,
+                y,
+                batch_size=BATCH_SIZE,
+                callbacks=[lambda_log_callback],
+                verbose=0,
+            )
+            self.assertTrue(
+                any(
+                    "custom_on_test_begin_executed" in log
+                    for log in logs.output
+                )
+            )
