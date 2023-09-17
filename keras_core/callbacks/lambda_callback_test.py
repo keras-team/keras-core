c72e412d92618cfa1ce65a3644ef4954e8fe31fc
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-17 14:34:27 +0000

    add tests to `lambda_callback_test`

--- keras_core/callbacks/lambda_callback_test.py
+++ keras_core/callbacks/lambda_callback_test.py
@@ -13,6 +13,7 @@ from keras_core.models.sequential import Sequential
 class LambdaCallbackTest(testing.TestCase):
     @pytest.mark.requires_trainable_backend
     def test_LambdaCallback(self):
+        """Test standard LambdaCallback functionalities with training."""
         BATCH_SIZE = 4
         model = Sequential(
             [layers.Input(shape=(2,), batch_size=BATCH_SIZE), layers.Dense(1)]
@@ -40,13 +41,18 @@ class LambdaCallbackTest(testing.TestCase):
                 epochs=5,
                 verbose=0,
             )
-            self.assertTrue(any("on_train_begin" in log for log in logs.output))
-            self.assertTrue(any("on_epoch_begin" in log for log in logs.output))
-            self.assertTrue(any("on_epoch_end" in log for log in logs.output))
-            self.assertTrue(any("on_train_end" in log for log in logs.output))
+            self.assertTrue
+            (any("on_train_begin" in log for log in logs.output))
+            self.assertTrue
+            (any("on_epoch_begin" in log for log in logs.output))
+            self.assertTrue
+            (any("on_epoch_end" in log for log in logs.output))
+            self.assertTrue
+            (any("on_train_end" in log for log in logs.output))
 
     @pytest.mark.requires_trainable_backend
     def test_LambdaCallback_with_batches(self):
+        """Test LambdaCallback's behavior with batch-level callbacks."""
         BATCH_SIZE = 4
         model = Sequential(
             [layers.Input(shape=(2,), batch_size=BATCH_SIZE), layers.Dense(1)]
@@ -83,6 +89,7 @@ class LambdaCallbackTest(testing.TestCase):
 
     @pytest.mark.requires_trainable_backend
     def test_LambdaCallback_with_kwargs(self):
+        """Test LambdaCallback's behavior with custom defined callback."""
         BATCH_SIZE = 4
         model = Sequential(
             [layers.Input(shape=(2,), batch_size=BATCH_SIZE), layers.Dense(1)]
@@ -96,7 +103,6 @@ class LambdaCallbackTest(testing.TestCase):
             x, y, batch_size=BATCH_SIZE, epochs=1, verbose=0
         )  # Train briefly for evaluation to work.
 
-        # Replacing lambda with a proper function definition
         def custom_on_test_begin(logs):
             logging.warning("custom_on_test_begin_executed")
 
@@ -116,4 +122,48 @@ class LambdaCallbackTest(testing.TestCase):
                     "custom_on_test_begin_executed" in log
                     for log in logs.output
                 )
+            )
+
+    @pytest.mark.requires_trainable_backend
+    def test_LambdaCallback_no_args(self):
+        """Test initializing LambdaCallback without any arguments."""
+        lambda_callback = callbacks.LambdaCallback()
+        self.assertIsInstance(lambda_callback, callbacks.LambdaCallback)
+
+    @pytest.mark.requires_trainable_backend
+    def test_LambdaCallback_with_additional_kwargs(self):
+        """Test initializing LambdaCallback with non-predefined kwargs."""
+
+        def custom_callback(logs):
+            pass
+
+        lambda_callback = callbacks.LambdaCallback(
+            custom_method=custom_callback
+        )
+        self.assertTrue(hasattr(lambda_callback, "custom_method"))
+
+    @pytest.mark.requires_trainable_backend
+    def test_LambdaCallback_during_prediction(self):
+        """Test LambdaCallback's functionality during model prediction."""
+        BATCH_SIZE = 4
+        model = Sequential(
+            [layers.Input(shape=(2,), batch_size=BATCH_SIZE), layers.Dense(1)]
+        )
+        model.compile(
+            optimizer=optimizers.SGD(), loss=losses.MeanSquaredError()
+        )
+        x = np.random.randn(16, 2)
+
+        def custom_on_predict_begin(logs):
+            logging.warning("on_predict_begin_executed")
+
+        lambda_callback = callbacks.LambdaCallback(
+            on_predict_begin=custom_on_predict_begin
+        )
+        with self.assertLogs(level="WARNING") as logs:
+            model.predict(
+                x, batch_size=BATCH_SIZE, callbacks=[lambda_callback], verbose=0
+            )
+            self.assertTrue(
+                any("on_predict_begin_executed" in log for log in logs.output)
             )
