874761844656a07998774faf459e8abaf3d12011
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-18 16:40:28 +0000

    Add tests in `saving_api_test.py`

--- keras_core/saving/saving_api_test.py
+++ keras_core/saving/saving_api_test.py
@@ -24,6 +24,7 @@ class SaveModelTests(test_case.TestCase):
         saving_api.save_model(self.model, self.filepath)
 
     def test_basic_saving(self):
+        """Test basic model saving and loading."""
         loaded_model = saving_api.load_model(self.filepath)
         x = np.random.uniform(size=(10, 3))
         self.assertTrue(
@@ -31,24 +32,28 @@ class SaveModelTests(test_case.TestCase):
         )
 
     def test_invalid_save_format(self):
+        """Test deprecated save_format argument."""
         with self.assertRaisesRegex(
             ValueError, "The `save_format` argument is deprecated"
         ):
             saving_api.save_model(self.model, "model.txt", save_format=True)
 
     def test_unsupported_arguments(self):
+        """Test unsupported argument during model save."""
         with self.assertRaisesRegex(
             ValueError, r"The following argument\(s\) are not supported"
         ):
             saving_api.save_model(self.model, self.filepath, random_arg=True)
 
     def test_save_h5_format(self):
+        """Test saving model in h5 format."""
         filepath_h5 = "test_model.h5"
         saving_api.save_model(self.model, filepath_h5)
         self.assertTrue(os.path.exists(filepath_h5))
         os.remove(filepath_h5)
 
     def test_save_unsupported_extension(self):
+        """Test saving model with unsupported extension."""
         with self.assertRaisesRegex(
             ValueError, "Invalid filepath extension for saving"
         ):
@@ -71,6 +76,7 @@ class LoadModelTests(test_case.TestCase):
         saving_api.save_model(self.model, self.filepath)
 
     def test_basic_load(self):
+        """Test basic model loading."""
         loaded_model = saving_api.load_model(self.filepath)
         x = np.random.uniform(size=(10, 3))
         self.assertTrue(
@@ -78,14 +84,17 @@ class LoadModelTests(test_case.TestCase):
         )
 
     def test_load_unsupported_format(self):
+        """Test loading model with unsupported format."""
         with self.assertRaisesRegex(ValueError, "File format not supported"):
             saving_api.load_model("model.pkl")
 
     def test_load_keras_not_zip(self):
+        """Test loading keras file that's not a zip."""
         with self.assertRaisesRegex(ValueError, "File not found"):
             saving_api.load_model("not_a_zip.keras")
 
     def test_load_h5_format(self):
+        """Test loading model in h5 format."""
         filepath_h5 = "test_model.h5"
         saving_api.save_model(self.model, filepath_h5)
         loaded_model = saving_api.load_model(filepath_h5)
@@ -95,11 +104,9 @@ class LoadModelTests(test_case.TestCase):
         )
         os.remove(filepath_h5)
 
-    def tearDown(self):
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
-
     def test_load_model_with_custom_objects(self):
+        """Test loading model with custom objects."""
+
         class CustomLayer(layers.Layer):
             def call(self, inputs):
                 return inputs
@@ -112,6 +119,10 @@ class LoadModelTests(test_case.TestCase):
         self.assertIsInstance(loaded_model.layers[0], CustomLayer)
         os.remove("custom_model.keras")
 
+    def tearDown(self):
+        if os.path.exists(self.filepath):
+            os.remove(self.filepath)
+
 
 class LoadWeightsTests(test_case.TestCase):
     def setUp(self):
@@ -123,6 +134,7 @@ class LoadWeightsTests(test_case.TestCase):
         )
 
     def test_load_keras_weights(self):
+        """Test loading keras weights."""
         filepath = "test_weights.weights.h5"
         self.model.save_weights(filepath)
         original_weights = self.model.get_weights()
@@ -131,20 +143,22 @@ class LoadWeightsTests(test_case.TestCase):
         for orig, loaded in zip(original_weights, loaded_weights):
             self.assertTrue(np.array_equal(orig, loaded))
 
-    def tearDown(self):
-        filepath = "test_weights.weights.h5"
-        if os.path.exists(filepath):
-            os.remove(filepath)
-
     def test_load_h5_weights_by_name(self):
+        """Test loading h5 weights by name."""
         filepath = "test_weights.weights.h5"
         self.model.save_weights(filepath)
         with self.assertRaisesRegex(ValueError, "Invalid keyword arguments"):
             self.model.load_weights(filepath, by_name=True)
 
     def test_load_weights_invalid_extension(self):
+        """Test loading weights with unsupported extension."""
         with self.assertRaisesRegex(ValueError, "File format not supported"):
             self.model.load_weights("invalid_extension.pkl")
+
+    def tearDown(self):
+        filepath = "test_weights.weights.h5"
+        if os.path.exists(filepath):
+            os.remove(filepath)
 
 
 class SaveModelTestsWarning(test_case.TestCase):
@@ -161,6 +175,7 @@ class SaveModelTestsWarning(test_case.TestCase):
         saving_api.save_model(self.model, self.filepath)
 
     def test_h5_deprecation_warning(self):
+        """Test deprecation warning for h5 format."""
         with mock.patch.object(logging, "warning") as mock_warn:
             saving_api.save_model(self.model, "test_model.h5")
             mock_warn.assert_called_once_with(
