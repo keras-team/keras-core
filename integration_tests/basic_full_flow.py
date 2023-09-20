f1830a4a7699423f7d9595fde8c93056243d394f
Neel Kovelamudi <nkovela@google.com>
2023-09-20 23:25:59 +0000

    Add basic flow script

---
+++ integration_tests/basic_full_flow.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+import keras_core as keras
+
+from keras_core import Model
+from keras_core import layers
+from keras_core import losses
+from keras_core import metrics
+from keras_core import optimizers
+
+
+class MyModel(Model):
+    def __init__(self, hidden_dim, output_dim):
+        super().__init__()
+        self.dense1 = layers.Dense(hidden_dim, activation="relu")
+        self.dense2 = layers.Dense(hidden_dim, activation="relu")
+        self.dense3 = layers.Dense(output_dim)
+
+    def call(self, x):
+        x = self.dense1(x)
+        x = self.dense2(x)
+        return self.dense3(x)
+
+
+model = MyModel(hidden_dim=256, output_dim=16)
+
+x = np.random.random((50000, 128))
+y = np.random.random((50000, 16))
+batch_size = 32
+epochs = 6
+
+model.compile(
+    optimizer=optimizers.SGD(learning_rate=0.001),
+    loss=losses.MeanSquaredError(),
+    metrics=[metrics.MeanSquaredError()],
+)
+history = model.fit(
+    x, y, batch_size=batch_size, epochs=epochs, validation_split=0.2
+)
+
+print("History:")
+print(history.history)
+
+model.summary()
+
+# Test saving model
+
+model.save("basic_model.keras")
+loaded_model keras.models.load_model("basic_model.keras")
+loaded_model.predict(x)
+
+# Test export model
+
+model.export("basic_model")
+revived_model = tf.saved_model.load("basic_model")
+revived_model(x)
