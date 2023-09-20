fe8bd58fd1872217fbea1386de26e20faa9cb584
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-20 05:37:09 +0000

    use `get_model` and `self.get_temp_dir`

--- keras_core/saving/saving_api_test.py
+++ keras_core/saving/saving_api_test.py
@@ -11,77 +11,76 @@ from keras_core.testing import test_case
 
 
 class SaveModelTests(test_case.TestCase):
-    def setUp(self):
-        self.model = Sequential(
+    def get_model(self):
+        return Sequential(
             [
                 layers.Dense(5, input_shape=(3,)),
                 layers.Softmax(),
-            ],
+            ]
         )
-        self.filepath = "test_model.keras"
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
-        saving_api.save_model(self.model, self.filepath)
 
     def test_basic_saving(self):
         """Test basic model saving and loading."""
-        loaded_model = saving_api.load_model(self.filepath)
+        model = self.get_model()
+        filepath = os.path.join(self.get_temp_dir(), "test_model.keras")
+        saving_api.save_model(model, filepath)
+
+        loaded_model = saving_api.load_model(filepath)
         x = np.random.uniform(size=(10, 3))
-        self.assertTrue(
-            np.allclose(self.model.predict(x), loaded_model.predict(x))
-        )
+        self.assertTrue(np.allclose(model.predict(x), loaded_model.predict(x)))
 
     def test_invalid_save_format(self):
         """Test deprecated save_format argument."""
+        model = self.get_model()
         with self.assertRaisesRegex(
             ValueError, "The `save_format` argument is deprecated"
         ):
-            saving_api.save_model(self.model, "model.txt", save_format=True)
+            saving_api.save_model(model, "model.txt", save_format=True)
 
     def test_unsupported_arguments(self):
         """Test unsupported argument during model save."""
+        model = self.get_model()
+        filepath = os.path.join(self.get_temp_dir(), "test_model.keras")
         with self.assertRaisesRegex(
             ValueError, r"The following argument\(s\) are not supported"
         ):
-            saving_api.save_model(self.model, self.filepath, random_arg=True)
+            saving_api.save_model(model, filepath, random_arg=True)
 
     def test_save_h5_format(self):
         """Test saving model in h5 format."""
-        filepath_h5 = "test_model.h5"
-        saving_api.save_model(self.model, filepath_h5)
+        model = self.get_model()
+        filepath_h5 = os.path.join(self.get_temp_dir(), "test_model.h5")
+        saving_api.save_model(model, filepath_h5)
         self.assertTrue(os.path.exists(filepath_h5))
         os.remove(filepath_h5)
 
     def test_save_unsupported_extension(self):
         """Test saving model with unsupported extension."""
+        model = self.get_model()
         with self.assertRaisesRegex(
             ValueError, "Invalid filepath extension for saving"
         ):
-            saving_api.save_model(self.model, "model.png")
-
-    def tearDown(self):
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
+            saving_api.save_model(model, "model.png")
 
 
 class LoadModelTests(test_case.TestCase):
-    def setUp(self):
-        self.model = Sequential(
+    def get_model(self):
+        return Sequential(
             [
                 layers.Dense(5, input_shape=(3,)),
                 layers.Softmax(),
-            ],
+            ]
         )
-        self.filepath = "test_model.keras"
-        saving_api.save_model(self.model, self.filepath)
 
     def test_basic_load(self):
         """Test basic model loading."""
-        loaded_model = saving_api.load_model(self.filepath)
+        model = self.get_model()
+        filepath = os.path.join(self.get_temp_dir(), "test_model.keras")
+        saving_api.save_model(model, filepath)
+
+        loaded_model = saving_api.load_model(filepath)
         x = np.random.uniform(size=(10, 3))
-        self.assertTrue(
-            np.allclose(self.model.predict(x), loaded_model.predict(x))
-        )
+        self.assertTrue(np.allclose(model.predict(x), loaded_model.predict(x)))
 
     def test_load_unsupported_format(self):
         """Test loading model with unsupported format."""
@@ -95,13 +94,12 @@ class LoadModelTests(test_case.TestCase):
 
     def test_load_h5_format(self):
         """Test loading model in h5 format."""
-        filepath_h5 = "test_model.h5"
-        saving_api.save_model(self.model, filepath_h5)
+        model = self.get_model()
+        filepath_h5 = os.path.join(self.get_temp_dir(), "test_model.h5")
+        saving_api.save_model(model, filepath_h5)
         loaded_model = saving_api.load_model(filepath_h5)
         x = np.random.uniform(size=(10, 3))
-        self.assertTrue(
-            np.allclose(self.model.predict(x), loaded_model.predict(x))
-        )
+        self.assertTrue(np.allclose(model.predict(x), loaded_model.predict(x)))
         os.remove(filepath_h5)
 
     def test_load_model_with_custom_objects(self):
@@ -112,79 +110,69 @@ class LoadModelTests(test_case.TestCase):
                 return inputs
 
         model = Sequential([CustomLayer(input_shape=(3,))])
-        model.save("custom_model.keras")
+        filepath = os.path.join(self.get_temp_dir(), "custom_model.keras")
+        model.save(filepath)
         loaded_model = saving_api.load_model(
-            "custom_model.keras", custom_objects={"CustomLayer": CustomLayer}
+            filepath, custom_objects={"CustomLayer": CustomLayer}
         )
         self.assertIsInstance(loaded_model.layers[0], CustomLayer)
-        os.remove("custom_model.keras")
-
-    def tearDown(self):
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
+        os.remove(filepath)
 
 
 class LoadWeightsTests(test_case.TestCase):
-    def setUp(self):
-        self.model = Sequential(
+    def get_model(self):
+        return Sequential(
             [
                 layers.Dense(5, input_shape=(3,)),
                 layers.Softmax(),
-            ],
+            ]
         )
 
     def test_load_keras_weights(self):
         """Test loading keras weights."""
-        filepath = "test_weights.weights.h5"
-        self.model.save_weights(filepath)
-        original_weights = self.model.get_weights()
-        self.model.load_weights(filepath)
-        loaded_weights = self.model.get_weights()
+        model = self.get_model()
+        filepath = os.path.join(self.get_temp_dir(), "test_weights.weights.h5")
+        model.save_weights(filepath)
+        original_weights = model.get_weights()
+        model.load_weights(filepath)
+        loaded_weights = model.get_weights()
         for orig, loaded in zip(original_weights, loaded_weights):
             self.assertTrue(np.array_equal(orig, loaded))
 
     def test_load_h5_weights_by_name(self):
         """Test loading h5 weights by name."""
-        filepath = "test_weights.weights.h5"
-        self.model.save_weights(filepath)
+        model = self.get_model()
+        filepath = os.path.join(self.get_temp_dir(), "test_weights.weights.h5")
+        model.save_weights(filepath)
         with self.assertRaisesRegex(ValueError, "Invalid keyword arguments"):
-            self.model.load_weights(filepath, by_name=True)
+            model.load_weights(filepath, by_name=True)
 
     def test_load_weights_invalid_extension(self):
         """Test loading weights with unsupported extension."""
+        model = self.get_model()
         with self.assertRaisesRegex(ValueError, "File format not supported"):
-            self.model.load_weights("invalid_extension.pkl")
-
-    def tearDown(self):
-        filepath = "test_weights.weights.h5"
-        if os.path.exists(filepath):
-            os.remove(filepath)
+            model.load_weights("invalid_extension.pkl")
 
 
 class SaveModelTestsWarning(test_case.TestCase):
-    def setUp(self):
-        self.model = Sequential(
+    def get_model(self):
+        return Sequential(
             [
                 layers.Dense(5, input_shape=(3,)),
                 layers.Softmax(),
-            ],
+            ]
         )
-        self.filepath = "test_model.keras"
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
-        saving_api.save_model(self.model, self.filepath)
 
     def test_h5_deprecation_warning(self):
         """Test deprecation warning for h5 format."""
+        model = self.get_model()
+        filepath = os.path.join(self.get_temp_dir(), "test_model.h5")
+
         with mock.patch.object(logging, "warning") as mock_warn:
-            saving_api.save_model(self.model, "test_model.h5")
+            saving_api.save_model(model, filepath)
             mock_warn.assert_called_once_with(
                 "You are saving your model as an HDF5 file via `model.save()`. "
                 "This file format is considered legacy. "
                 "We recommend using instead the native Keras format, "
                 "e.g. `model.save('my_model.keras')`."
             )
-
-    def tearDown(self):
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
