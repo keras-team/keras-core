6841f50e94af32fb19dd87b728d8d07267d77bfa
HongYu <20734616+james77777778@users.noreply.github.com>
2023-09-21 04:55:31 +0000

    Fix DropoutRNNCell test

--- keras_core/layers/rnn/dropout_rnn_cell_test.py
+++ keras_core/layers/rnn/dropout_rnn_cell_test.py
@@ -64,4 +64,22 @@ class DropoutRNNCellTest(testing.TestCase):
             expected_num_non_trainable_weights=0,
             expected_num_non_trainable_variables=1,
             supports_masking=True,
+            run_mixed_precision_check=False,
+        )
+
+        # custom mixed_float16 check
+        self.run_layer_test(
+            layers.RNN,
+            init_kwargs={
+                "cell": RNNCellWithDropout(5, seed=1337, dtype="mixed_float16"),
+                "dtype": "mixed_float16",
+            },
+            input_shape=(3, 2, 4),
+            call_kwargs={"training": True},
+            expected_output_shape=(3, 5),
+            expected_num_trainable_weights=2,
+            expected_num_non_trainable_weights=0,
+            expected_num_non_trainable_variables=1,
+            supports_masking=True,
+            run_mixed_precision_check=False,
         )
